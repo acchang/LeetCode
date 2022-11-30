ed1e74fcfa9ee7d171f392818345eae0618ff4bd
Andrew Chang <acchang4@gmail.com>
2022-11-30 09:20:29 -0500

    lessons

--- 1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences.js
@@ -10,10 +10,19 @@ var uniqueOccurrences = function(arr) {
     let valuesArr = Object.values(objectOfUniques)
     // use indexOf/lastIndexOf to find dupes
     let passing = true
-        valuesArr.forEach(function(x) {
-        if (valuesArr.indexOf(x) != valuesArr.lastIndexOf(x)) {
-            passing = false;
-        }
-    });
+        valuesArr.forEach((x) => {
+        if (valuesArr.indexOf(x) != valuesArr.lastIndexOf(x)) {passing = false; }
+        });
     return passing
 };
+
+/*
+https://leetcode.com/problems/unique-number-of-occurrences/submissions/
+
+lessons:
+cannot return in the middle of forEach, better to set a variable.
+
+Object.values can turn object into array of values.
+
+Use brackets to assign object keys when working with variables:
+objectOfUniques[uniques[i]]
