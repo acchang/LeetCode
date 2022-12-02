2b13bd5235241826fa01d5651b8164d1338e75ac
Andrew Chang <acchang4@gmail.com>
2022-12-02 07:26:46 -0500

    solved, but slow

--- 1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close.js
@@ -9,30 +9,19 @@ If above 2 conditions are satisfied then just swapping will get us the word2 fro
 
 
 var closeStrings = function(word1, word2) {
-    if (word1.length !== word2.length){return false}
     let word1Uniques = [... new Set(word1)].sort();
     let word2Uniques = [... new Set(word2)].sort();
     let objectOfWord1Uniques = {};
     let objectOfWord2Uniques = {};
     for (let i=0;i<word1Uniques.length;i++){ objectOfWord1Uniques[word1Uniques[i]] = word1.split("").filter((x) => x == word1Uniques[i]).length }
     for (let i=0;i<word2Uniques.length;i++){ objectOfWord2Uniques[word2Uniques[i]] = word2.split("").filter((x) => x == word2Uniques[i]).length }
-
-    for (let k=0;k<Object.values(objectOfWord1Uniques).length;k++){
-        if ( Object.values(objectOfWord1Uniques)[k] != Object.values(objectOfWord2Uniques)[k] ){
-            console.log(k);
-            return false
-        }
-    }
-    for (let l=0;l<Object.keys(objectOfWord1Uniques).length;l++){
-        if ( Object.keys(objectOfWord1Uniques)[l] != Object.keys(objectOfWord2Uniques)[l] ){
-            return false
-        }
-    }
+    if (Object.keys(objectOfWord1Uniques).join("") != Object.keys(objectOfWord2Uniques).join("")) {return false};
+    if (Object.values(objectOfWord1Uniques).sort().join("") != Object.values(objectOfWord2Uniques).sort().join("")) {return false};
     return true
 };
 
-
-// It won't allow this, maybe bc alters word?
+// It won't allow this on "abc, cba" maybe bc alters word, also would fail in frequency mismatch
+// ie abbccc, caabbb
 
 var closeStrings = function(word1, word2) {
     let new1 = word1.split("").sort()
