6dbea4ae94a40ddb33fe9a8d0dc6fe7ba1e0d839
Andrew Chang <acchang4@gmail.com>
2022-12-03 06:22:50 -0500

    now to just sort by value and repeat

--- 451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency.js
@@ -1,16 +1,18 @@
 var frequencySort = function(s) {
+        console.log(s)
+        let answerObj = {}
+        for (let i=0;i<s.length;i++){
+            if (s[i] != s[i-1]){ answerObj[s[i]] = 1 }
+            if (s[i] == s[i-1]){ newValue = answerObj[s[i]] +1
+            answerObj[s[i]] = newValue }
+        }
+        console.log(answerObj);
+    };
+
+
     // Parameters: uppercase and lowercase English letters and digits.
     // Returns: same letters as input s
     // Examples: Given
     // PseudoCode:
     // create hashmap of letters
     // sort by value in descending order, return keys times repeat
-    let keys = [... new Set(s)]
-    // skip this, create hashmap with a loop
-    let answerObj = {}
-    for (let i=0;i<s.length;i++){
-        if (s[i] != s[i+1]){answerObj.s[i] = 1};
-        if (s[i] == s[i+1]){answerObj.s[i] += 1};
-    }
-    console.log(answerObj);
-};
