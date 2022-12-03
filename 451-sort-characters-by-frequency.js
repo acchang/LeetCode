e1e67ffe4e5e1b140872b88029a4636636c81e83
Andrew Chang <acchang4@gmail.com>
2022-12-03 06:41:15 -0500

    output the answer

--- 451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency.js
@@ -1,13 +1,22 @@
 var frequencySort = function(s) {
-        console.log(s)
-        let answerObj = {}
-        for (let i=0;i<s.length;i++){
-            if (s[i] != s[i-1]){ answerObj[s[i]] = 1 }
-            if (s[i] == s[i-1]){ newValue = answerObj[s[i]] +1
-            answerObj[s[i]] = newValue }
-        }
-        console.log(answerObj);
-    };
+    console.log(s)
+    
+    let answerObj = {}
+    for (let i=0;i<s.length;i++){
+        if (s[i] != s[i-1]){ answerObj[s[i]] = 1 }
+        if (s[i] == s[i-1]){ newValue = answerObj[s[i]] +1
+        answerObj[s[i]] = newValue }
+    }
+    
+    let sortArray = []
+    for (var letter in answerObj) {
+    sortArray.push([letter, answerObj[letter]])};
+
+    
+    
+    
+    
+};
 
 
     // Parameters: uppercase and lowercase English letters and digits.
