54731718f1e00992747e1cec8285b0793dcee8df
Andrew Chang <acchang4@gmail.com>
2022-12-04 05:50:51 -0500

    last is zero

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,16 +1,12 @@
-var minimumAverageDifference = function(nums) {
-    console.log(nums)
-    let lowest = Infinity;
-    let lowestIndex = ""
-    for (let i=nums.length-1; i>-1; i--){
-        let first = nums.slice(0,i+1);
-        let firstAvg = first.reduce((a, b) => a + b, 0) / first.length;
-        let second = nums.slice(i+1);
-        let secondAvg = second.reduce((a, b) => a + b, 0) / second.length;
-        let result = Math.abs(Math.floor(firstAvg - secondAvg));
-        console.log("r:" + result)
-        if (result<=lowest){lowest=result, lowestIndex=i}
-        console.log("L:" + lowest, lowestIndex)
-    }
-    return lowestIndex
+for (let i=nums.length-1; i>-1; i--){
+    let first = nums.slice(0,i+1);
+    let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
+    let second = nums.slice(i+1);
+    let secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length);
+    let result = Math.abs(firstAvg - secondAvg);
+    console.log("r:" + i, result, firstAvg, secondAvg)
+    if (result<=lowest){lowest=result, lowestIndex=i}
+    console.log("L:" + lowest, lowestIndex)
+}
+return lowestIndex
 };
