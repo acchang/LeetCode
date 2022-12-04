87898f77f233a70af3c7fa96fcd7994f995a219b
Andrew Chang <acchang4@gmail.com>
2022-12-04 06:19:35 -0500

    still too slow, need math

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,13 +1,11 @@
 var minimumAverageDifference = function(nums) {
-    let lowest = Infinity;
-    let lowestIndex = ""
-    for (let i=nums.length-1; i>-1; i--){
+    let averages = []
+    for (let i=0; i<nums.length; i++){
         let first = nums.slice(0,i+1);
         let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
         let second = nums.slice(i+1);
         second == 0 ? secondAvg = 0 : secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length)
-        let result = Math.abs(firstAvg - secondAvg);
-        if (result<=lowest){lowest=result, lowestIndex=i}
+        averages.push(Math.abs(firstAvg - secondAvg))        
     }
-    return lowestIndex
+    return averages.indexOf(Math.min(...averages))
 };
