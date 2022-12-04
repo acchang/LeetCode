4488439a6ce05226794e8325203c693285d46896
Andrew Chang <acchang4@gmail.com>
2022-12-04 07:02:03 -0500

    solutions basically same, break it up

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,10 +1,6 @@
-
-
-// array too long, work with vectors.
-
 var minimumAverageDifference = function(nums) {
     let lowest = Infinity;
-    let lowestIndex = ""
+    let lowestIndex = -1
     for (let i=nums.length-1; i>-1; i--){
         let first = nums.slice(0,i+1);
         let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
@@ -14,4 +10,65 @@ var minimumAverageDifference = function(nums) {
         if (result<=lowest){lowest=result, lowestIndex=i}
     }
     return lowestIndex
+};
+
+// Why does the above time out and these do not?
+
+const minimumAverageDifference = function(nums) {
+	const n = nums.length;
+	const leftSum = new Array(n).fill(0);
+	const rightSum = new Array(n).fill(0);
+	
+	leftSum[0] = nums[0];
+	for (let i = 1; i < n; i++) 
+		leftSum[i] = leftSum[i - 1] + nums[i];
+	
+	rightSum[n - 1] = nums[n - 1];
+	for (let i = n - 2; i >= 0; i--)
+		rightSum[i] = rightSum[i + 1] + nums[i];
+	
+	let minAvgDiff = Infinity;
+	let answer = -1;
+	
+	for (let i = 0; i < n; i++) {
+		const leftAvg = Math.floor(leftSum[i] / (i + 1));
+		const rightAvg = i == n - 1  ? 0 : Math.floor(rightSum[i + 1] / (n - i - 1));
+		const avgDiff = Math.abs(leftAvg - rightAvg);
+		
+		if (avgDiff < minAvgDiff) {
+			minAvgDiff = avgDiff;
+			answer = i;
+		}
+	}
+	
+	return answer;
+};
+
+
+const minimumAverageDifference = function(nums) {
+	const n = nums.length;
+	let leftSum = 0;
+	let rightSum = 0;
+
+	for (let i = 0; i < n; i++) 
+		rightSum += nums[i];
+
+	let minAvgDiff = Infinity;
+	let answer = -1;
+
+	for (let i = 0; i < n; i++) {
+		leftSum += nums[i];
+		rightSum -= nums[i];
+
+		const leftAvg = Math.floor(leftSum / (i + 1));
+		const rightAvg = i == n - 1  ? 0 : Math.floor(rightSum / (n - i - 1));
+		const avgDiff = Math.abs(leftAvg - rightAvg);
+		
+		if (avgDiff < minAvgDiff) {
+			minAvgDiff = avgDiff;
+			answer = i;
+		}
+	}
+
+	return answer;
 };
