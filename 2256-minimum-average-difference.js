5b8a5357ca90599f803d4d4100db3df328f301f7
Andrew Chang <acchang4@gmail.com>
2022-12-04 08:28:05 -0500

    no slicing

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,3 +1,42 @@
+// trying mine without the slice.
+
+var minimumAverageDifference = function(nums) {
+    let lowest = Infinity;
+    let lowestIndex = -1
+    for (let i=nums.length-1; i>-1; i--){
+        let first = nums.slice(0,i+1);
+        let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
+        let second = nums.slice(i+1);
+        second == 0 ? secondAvg = 0 : secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length)
+        let result = Math.abs(firstAvg - secondAvg);
+        if (result<=lowest){lowest=result, lowestIndex=i}
+    }
+    return lowestIndex
+};
+
+
+
+// lol I found Paul: https://leetcode.com/problems/minimum-average-difference/discuss/1994789/Javascript-straightforward-with-explanation
+
+var minimumAverageDifference = function(nums) {
+    let min = Number.MAX_SAFE_INTEGER;
+    let index = 0;
+    for (let i = 0, left=0, right=nums.reduce((sum, curr)=>sum+curr, 0), curr; i<nums.length; i++) {
+        left+=nums[i];
+        right-=nums[i];
+        curr = Math.abs(Math.floor(left/(i+1))-Math.floor(right/(nums.length-i-1)||0))
+        if (min >curr) {
+            min = curr
+            index = i;
+        }
+    }
+    return index; 
+ };
+
+
+
+// Tried this, are the fills the issue or the slice?
+
 const minimumAverageDifference = function(nums) {
 	const n = nums.length;
 	
@@ -6,17 +45,27 @@ const minimumAverageDifference = function(nums) {
         leftend = nums.slice(0,i);
         leftSumsArray.push(leftend.reduce((a,b) => a+b, 0))
     }
-    
+
     let rightSumsArray = []
     for (let i=0; i<n; i++){
         rightend = nums.slice(i,n);
         rightSumsArray.push(rightend.reduce((a,b) => a+b, 0))
     }
-
-}
-
-
-
+    
+    let minAvgDiff = Infinity
+    
+    for (let i = 0; i < n; i++) {
+		const leftAvg = Math.floor(leftSumsArray[i] / (i + 1));
+		const rightAvg = i == n - 1  ? 0 : Math.floor(rightSumsArray[i + 1] / (n - i - 1));
+		const avgDiff = Math.abs(leftAvg - rightAvg);
+		
+		if (avgDiff < minAvgDiff) {
+			minAvgDiff = avgDiff;
+			answer = i;}
+    }
+        
+        return answer;
+    };
 
 // why does the below time out?
 
@@ -35,6 +84,7 @@ var minimumAverageDifference = function(nums) {
 };
 
 // Why does the above time out and these do not? Was it done for memory? What lessons can I take, break it up?
+// https://leetcode.com/problems/minimum-average-difference/discuss/2875404/JavaScript-or-2-Approaches-with-Explanation-Clean-Code
 
 const minimumAverageDifference = function(nums) {
 	const n = nums.length;
