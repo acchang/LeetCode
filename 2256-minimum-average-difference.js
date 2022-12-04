d335cd5e6f8642d2f41f780aea7a030c9637f0a2
Andrew Chang <acchang4@gmail.com>
2022-12-04 06:36:13 -0500

    best so far

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,11 +1,13 @@
 var minimumAverageDifference = function(nums) {
-    let averages = []
-    for (let i=0; i<nums.length; i++){
+    let lowest = Infinity;
+    let lowestIndex = ""
+    for (let i=nums.length-1; i>-1; i--){
         let first = nums.slice(0,i+1);
         let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
         let second = nums.slice(i+1);
         second == 0 ? secondAvg = 0 : secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length)
-        averages.push(Math.abs(firstAvg - secondAvg))        
+        let result = Math.abs(firstAvg - secondAvg);
+        if (result<=lowest){lowest=result, lowestIndex=i}
     }
-    return averages.indexOf(Math.min(...averages))
+    return lowestIndex
 };
