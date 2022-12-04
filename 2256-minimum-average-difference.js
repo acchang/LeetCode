e5e9942cf5cc82fe92983a4b4dc0cb604e181d8d
Andrew Chang <acchang4@gmail.com>
2022-12-04 06:12:46 -0500

    too brute force

--- 2256-minimum-average-difference.js
+++ 2256-minimum-average-difference.js
@@ -1,12 +1,13 @@
-for (let i=nums.length-1; i>-1; i--){
-    let first = nums.slice(0,i+1);
-    let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
-    let second = nums.slice(i+1);
-    let secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length);
-    let result = Math.abs(firstAvg - secondAvg);
-    console.log("r:" + i, result, firstAvg, secondAvg)
-    if (result<=lowest){lowest=result, lowestIndex=i}
-    console.log("L:" + lowest, lowestIndex)
-}
-return lowestIndex
+var minimumAverageDifference = function(nums) {
+    let lowest = Infinity;
+    let lowestIndex = ""
+    for (let i=nums.length-1; i>-1; i--){
+        let first = nums.slice(0,i+1);
+        let firstAvg = Math.floor(first.reduce((a, b) => a + b, 0) / first.length);
+        let second = nums.slice(i+1);
+        second == 0 ? secondAvg = 0 : secondAvg = Math.floor(second.reduce((a, b) => a + b, 0) / second.length)
+        let result = Math.abs(firstAvg - secondAvg);
+        if (result<=lowest){lowest=result, lowestIndex=i}
+    }
+    return lowestIndex
 };
