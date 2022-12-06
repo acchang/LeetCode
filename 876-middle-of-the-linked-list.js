5d91572807b0825f93848bd2b200457889de837a
Andrew Chang <acchang4@gmail.com>
2022-12-06 06:32:31 -0500

    why one bad test case?

--- 876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list.js
@@ -9,8 +9,23 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var middleNode = function(head) {
-    return head.length
-    if (head.length % 2 != 0) {return head.splice(2)}
-    if (head.length % 2 === 0) {return head.splice(head.length/2)}
-};
+ 
+
+ var twoSum = function(nums, target) {
+    let matches = []
+    let pair = []
+    let answer = []
+    for (i=0;i<nums.length;i++){
+        pair.push(nums[i], target-nums[i])
+        matches.push(pair)
+        pair = []
+    }
+    console.log(matches)
+    for (j=0;matches.length;j++) {
+        if (matches[j].every(x => {return nums.includes(x)})){
+            answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
+            break
+        }
+    }
+    return answer
+ }
