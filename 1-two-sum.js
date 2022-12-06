0649282a24b39ffad13e6aec5dadc32d7d799ce1
Andrew Chang <acchang4@gmail.com>
2022-12-06 06:47:16 -0500

    just test case 2

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -3,15 +3,24 @@
  * @param {number} target
  * @return {number[]}
  */
+ 
  var twoSum = function(nums, target) {
-    console.log(nums,target)
     let matches = []
     let pair = []
+    let answer = []
     for (i=0;i<nums.length;i++){
-        let diff = target-nums[i]
-        pair.push(nums[i], diff)
+        pair.push(nums[i], target-nums[i])
         matches.push(pair)
         pair = []
     }
-    return matches
-};
+    console.log(matches)
+    for (j=0;matches.length;j++) {
+
+        if (matches[j].every(x => {return nums.includes(x)})){
+            answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
+            break
+        }
+        
+    }
+    return answer
+ }
