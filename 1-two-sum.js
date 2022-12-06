25bd08473a1885401a0f0166317489956c7d6051
Andrew Chang <acchang4@gmail.com>
2022-12-06 14:30:48 -0500

    retry paul's

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -9,14 +9,14 @@
     let pair = []
     let answer = []
     for (i=0;i<nums.length;i++){
-        pair.push(nums[i], target-nums[i])
+        if (nums[i] !=  target-nums[i]) {pair.push(nums[i], target-nums[i])}
         matches.push(pair)
         pair = []
     }
-    console.log(matches)
+
     for (j=0;matches.length;j++) {
 
-        if (matches[j].every(x => {return nums.includes(x)})){
+        if (matches[j].every(x => {return nums.includes(x)}) && matches[j][0] != matches[j][1]){
             console.log(matches[j])
             answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
             break
--- 451-sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency.js
@@ -20,9 +20,6 @@ var frequencySort = function(s) {
 // https://leetcode.com/problems/sort-characters-by-frequency/submissions/
 // https://leetcode.com/submissions/detail/853874436/
 
-
-
-
     // Parameters: uppercase and lowercase English letters and digits.
     // Returns: same letters as input s
     // Examples: Given
@@ -42,3 +39,45 @@ var frequencySort = function(s) {
         return sortedArr.reduce((acc, cur) => acc + cur.repeat(charMap[cur]) ,'')
         // output with repeat
     };
+
+// paul's solution:
+// https://leetcode.com/problems/determine-if-two-strings-are-close/solutions/2870310/javascript-easy-to-read-code-with-explanation-70-faster-98-less-mem/?q=paultman&orderBy=most_relevant
+
+function word2hash(word) {
+    const hash = new Map();
+    for (let i = 0; i<word.length; i++)
+        hash.set(word[i], (hash.get(word[i])|| 0) + 1);
+    return hash; 
+}
+
+function checkSameChars(word1, word2) {
+    if (word1.length != word2.length) return false;
+    for (let i = 0; i<word1.length; i++)
+        if (!word2.includes(word1[i])) return false;
+    return true;
+}
+
+function checkSameCounts(word1, word2) {
+    if (word1.length != word2.length) return false;
+    for (let index, i = 0; i<word1.length; i++) {
+      index = word2.indexOf(word1[i]);
+      // i=0, word1[i]='a'=>1, indexOf(word1[i]) =0, 
+      console.log(i, word1[i], word2.indexOf(word1[i]), word2[index])
+      if (index === -1) return false;
+    // ie does word1[i] exist in word2? if it does not then returns -1
+      word2[index] = -2;
+    }
+    return true;
+}
+
+var closeStrings = function(word1, word2) {
+    console.log(word1, word2)
+    if (word1.length != word2.length) return false;
+    word1 = word2hash(word1);
+    console.log(word1)
+    word2 = word2hash(word2);
+    console.log(word2)
+    return checkSameChars([...word1.keys()], [...word2.keys()]) && checkSameCounts([...word1.values()], [...word2.values()]);
+}
+
+Why is the hash =>
