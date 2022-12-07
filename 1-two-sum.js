5ebbe4a6391a9fb0dca94713fddd47e5258bcc4f
Andrew Chang <acchang4@gmail.com>
2022-12-06 21:32:13 -0500

    accepted

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -5,23 +5,42 @@
  */
  
  var twoSum = function(nums, target) {
-    let matches = []
-    let pair = []
     let answer = []
-    for (i=0;i<nums.length;i++){
-        if (nums[i] !=  target-nums[i]) {pair.push(nums[i], target-nums[i])}
-        matches.push(pair)
-        pair = []
-    }
+   const map = {}
+   for (const num of nums){
+       map[num] = target-num
+   }
+   // hashmap of nums and diffs
+   console.log(map)
 
-    for (j=0;matches.length;j++) {
+   for (const num of nums){
+       if (nums.includes(map[num]) && 
+       nums.indexOf(num) != nums.lastIndexOf(map[num])){
+           answer.push( nums.indexOf(num), nums.lastIndexOf(map[num]))
+           return answer
+       }
+   }
+};
 
-        if (matches[j].every(x => {return nums.includes(x)}) && matches[j][0] != matches[j][1]){
-            console.log(matches[j])
-            answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
-            break
-        }
+    
+    
+//     let matches = []
+//     let pair = []
+//     let answer = []
+//     for (i=0;i<nums.length;i++){
+//         if (nums[i] !=  target-nums[i]) {pair.push(nums[i], target-nums[i])}
+//         matches.push(pair)
+//         pair = []
+//     }
+
+//     for (j=0;matches.length;j++) {
+
+//         if (matches[j].every(x => {return nums.includes(x)}) && matches[j][0] != matches[j][1]){
+//             console.log(matches[j])
+//             answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
+//             break
+//         }
         
-    }
-    return answer
- }
+//     }
+//     return answer
+//  }
