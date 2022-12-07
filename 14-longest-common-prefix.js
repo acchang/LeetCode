255408ca17e2d75a0012034881953239cee1efc0
Andrew Chang <acchang4@gmail.com>
2022-12-07 18:50:24 -0500

    98% runtime, 47% memory

--- 14-longest-common-prefix.js
+++ 14-longest-common-prefix.js
@@ -3,6 +3,21 @@
  * @return {string}
  */
 
+  var longestCommonPrefix = function(strs) {
+     let prefix = ""
+     for (let i=0;i<strs[0].length; i++){
+         if (strs.every((x) => x[i] === strs[0][i] )){
+             prefix += strs[0][i]
+         }
+         else break
+     }
+         return prefix
+ };
+
+
+
+// above is best: 98/47
+
  var longestCommonPrefix = function(strs) {
     let prefix = ""
     let shortest =   strs.reduce(function(a, b) {
@@ -15,4 +30,27 @@
         else break
     }
         return prefix
+};
+
+// mine is 30% runtime, 60% memory
+// this claims 83/92 but I get 15/64
+// it's a nested loop so unlikely so high
+// https://leetcode.com/problems/longest-common-prefix/solutions/2844536/javascript/?q=javascript&orderBy=most_relevant
+// but it's a good idea to avoid the sort
+
+var longestCommonPrefix = function(strs) {
+    if(!strs.length)return ''
+    let prefix =''
+    const firstElement = strs[0]
+    for(let i = 0; i < firstElement.length;i++){
+        let pfx = !prefix ? firstElement[i] : prefix + firstElement[i] 
+        for(let j = 1; j < strs.length; j++){
+            if(!strs[j].startsWith(pfx)){
+                return prefix;
+            }
+        }
+        prefix = pfx;
+    }
+    
+    return prefix;
 };
