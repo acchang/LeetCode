b08f8098b1a538856bd97b73eac077cbd051f983
Andrew Chang <acchang4@gmail.com>
2022-12-07 18:32:30 -0500

    good on memory, bad on runtime

--- 14-longest-common-prefix.js
+++ 14-longest-common-prefix.js
@@ -9,13 +9,10 @@
     return a.length <= b.length ? a : b;
   })
     for (let i=0;i<shortest.length; i++){
-        console.log(i)
         if (strs.every((x) => x[i] === shortest[i] )){
             prefix += shortest[i]
-            console.log(shortest[i], prefix)
         }
         else break
     }
         return prefix
 };
-
