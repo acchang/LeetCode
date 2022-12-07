4ad928c1acd62e2b600d4cb09aee6ffeda775b1f
Andrew Chang <acchang4@gmail.com>
2022-12-06 22:29:16 -0500

    better solutions

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -22,25 +22,51 @@
    }
 };
 
-    
-    
-//     let matches = []
-//     let pair = []
-//     let answer = []
-//     for (i=0;i<nums.length;i++){
-//         if (nums[i] !=  target-nums[i]) {pair.push(nums[i], target-nums[i])}
-//         matches.push(pair)
-//         pair = []
-//     }
+ 
+// alternatives:
 
-//     for (j=0;matches.length;j++) {
+var twoSum = function(nums, target) {
+    const indices = {};
 
-//         if (matches[j].every(x => {return nums.includes(x)}) && matches[j][0] != matches[j][1]){
-//             console.log(matches[j])
-//             answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
-//             break
-//         }
-        
-//     }
-//     return answer
-//  }
+    nums.forEach((item, index) => {
+        indices[item] = index
+    });
+    console.log(indices)
+// each nums is paired with its index
+
+    for (let index = 0; index < nums.length; index++) {
+        const complement = target - nums[index];
+        // as you loop through nums, get the complement
+
+        if (indices[complement] !== undefined && indices[complement] !== index) {
+        // if the complement exists in the object indices, that is, if it is assigned an index, and that index is not the current index 
+            return [index, indices[complement]]
+        }
+    }
+};
+
+
+// uses has, set, get:
+//  The has() method returns a boolean indicating whether an element with the specified key exists or not
+
+// In JavaScript, a setter can be used to execute a function whenever a specified property is attempted to be changed. Setters are most often used in conjunction with getters to create a type of pseudo-property. It is not possible to simultaneously have a setter on a property that holds an actual value.
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
+
+
+
+
+var twoSum = function(nums, target) {
+    const indices = new Map();
+
+    for (let index = 0; index < nums.length; index++) {
+        const complement = target - nums[index];
+
+        if (indices.has(complement)) {
+            return [indices.get(complement), index]
+        }
+
+        indices.set(nums[index], index)
+    }
+};
+
