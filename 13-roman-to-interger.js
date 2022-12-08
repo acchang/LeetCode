756d897121e54bcc5f8d7ca52e1f1a5fa749dea6
Andrew Chang <acchang4@gmail.com>
2022-12-08 11:58:47 -0500

    86%/67%

--- 13-roman-to-interger.js
+++ 13-roman-to-interger.js
@@ -6,8 +6,7 @@ var romanToInt = function(s) {
     let answer = 0;
     let hash = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};
      for (let i=0 ;i<s.length-1; i++){
-        if ( hash[s[i]] >= hash[s[i+1]]){ answer += hash[s[i]] };
-        if ( hash[s[i]] < hash[s[i+1]] ){ answer += hash[s[i]] * -1 };
+        if ( hash[s[i]] < hash[s[i+1]] ){ answer += hash[s[i]] * -1 } else {answer += hash[s[i]]}
     }
     return answer + hash[s[s.length-1]]
 };
