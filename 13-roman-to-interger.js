e59f3e79d022947e78621bf4f23f6dbc9df6bde6
Andrew Chang <acchang4@gmail.com>
2022-12-08 12:02:14 -0500

    even better

--- 13-roman-to-interger.js
+++ 13-roman-to-interger.js
@@ -10,3 +10,19 @@ var romanToInt = function(s) {
     }
     return answer + hash[s[s.length-1]]
 };
+
+
+/**
+ * Runtime: 128 ms, faster than 95.62% of JavaScript online submissions for Roman to Integer.
+ * Memory Usage: 39.7 MB, less than 61.70% of JavaScript online submissions for Roman to Integer.
+ */
+var romanToInt = function(s) {
+    let hash ={I:1, V:5, X:10, L:50, C:100, D:500, M:1000}
+    let ans = 0, cur = 0, prev = 0, j = s.length - 1
+    for( ; j >= 0; j-=1){
+        cur = hash[s[j]]
+        cur < prev ? ans -= cur : ans += cur
+        prev = cur
+    }
+    return ans;    
+};
