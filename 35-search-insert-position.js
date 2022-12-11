9684db619ace4baca103f17c6141e798c0b514fa
Andrew Chang <acchang4@gmail.com>
2022-12-11 07:44:33 -0500

    added solutions

--- 35-search-insert-position.js
+++ 35-search-insert-position.js
@@ -22,4 +22,44 @@ Memory
 Beats
 21.16%
 
+This one is very clever, log search:
+Runtime
+92 ms
+Beats
+54.34%
+Memory
+42.1 MB
+Beats
+69.64%*/
+
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while(start < end){
+        let mid = Math.floor((start+end)/2);
+        if (nums[mid] === target) return mid;
+        nums[mid] > target ? end = mid : start = mid + 1;
+    }
+    if (start === end){
+        return target <= nums[start] ? start : start + 1;
+    }
+};
+
+/* very simple but slow: Runtime
+111 ms
+Beats
+12.71%
+Memory
+42.5 MB
+Beats
+21.16% 
+*/
+
+var searchInsert = function(nums, target) {
+    let i = 0;
+    while (nums[i] < target) { i++ };
+    return i  
+};
+
 
