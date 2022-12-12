c18665e2583587f63fcddc06e26ea985ac9daf7d
Andrew Chang <acchang4@gmail.com>
2022-12-11 20:37:09 -0500

    https://leetcode.com/problems/middle-of-the-linked-list/solutions/2878706/topic/

--- 876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list.js
@@ -9,4 +9,15 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- 
+
+// from: https://leetcode.com/problems/middle-of-the-linked-list/solutions/2878706/topic/
+
+
+var middleNode = function(head) {
+    let fast = head;
+    while (fast != null && fast.next != null) {
+        head = head.next;
+        fast = fast.next.next;
+    }
+    return head;
+};
