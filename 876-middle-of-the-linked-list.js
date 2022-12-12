9b93f35a1974fc82c1db36159c40cbbc94f9245f
Andrew Chang <acchang4@gmail.com>
2022-12-11 19:21:57 -0500

    try

--- 876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list.js
@@ -10,22 +10,3 @@
  * @return {ListNode}
  */
  
-
- var twoSum = function(nums, target) {
-    let matches = []
-    let pair = []
-    let answer = []
-    for (i=0;i<nums.length;i++){
-        pair.push(nums[i], target-nums[i])
-        matches.push(pair)
-        pair = []
-    }
-    console.log(matches)
-    for (j=0;matches.length;j++) {
-        if (matches[j].every(x => {return nums.includes(x)})){
-            answer.push(nums.indexOf(matches[j][0]), nums.lastIndexOf(matches[j][1]))
-            break
-        }
-    }
-    return answer
- }
