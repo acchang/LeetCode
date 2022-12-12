d5ad18070d7960326d0170bd010a61f124c7567f
Andrew Chang <acchang4@gmail.com>
2022-12-12 06:32:36 -0500

    order of ops

--- 71-simplify-path.js
+++ 71-simplify-path.js
@@ -5,6 +5,23 @@
 var simplifyPath = function(path) {
     let answer = ""
     if (path[path.length]="/"){answer = path.slice(0, -1)}
+    for (let i=answer.length;i>0;i--){
+        if (answer[i] == "." && answer[i-1] == "."){answer = answer.slice([i-2])
+        }
+    }
     answer = answer.split("").filter((x) => x != ".")
-    return answer.join("")
+    for (let j=0;j<answer.length;j++){
+        if (answer[j] == "/" && answer[j+1] == "/"){answer.splice([j],1)
+        j--
+        }
+    }
+    answer = answer.join("")
+    // for (let j=answer.length;j>0;j--){
+    //     if (answer[j] == "/"){answer = answer.slice(j)}
+    // }
+    return answer
 };
+
+// "/a/./b/../../c/" should be "/c"
+// "/home//foo/" should be "/home/foo"
+
