953e625222699e73196a8a8ccac0910577a95af6
Andrew Chang <acchang4@gmail.com>
2022-12-12 06:49:26 -0500

    no clear order

--- 71-simplify-path.js
+++ 71-simplify-path.js
@@ -3,25 +3,56 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    let answer = ""
-    if (path[path.length]="/"){answer = path.slice(0, -1)}
-    for (let i=answer.length;i>0;i--){
-        if (answer[i] == "." && answer[i-1] == "."){answer = answer.slice([i-2])
-        }
+
+    // get rid of '..'
+    for (let i=path.length;i>0;i--){
+        if (path[i] == "." && path[i-1] == "."){path = path.slice([i-2])}
     }
-    answer = answer.split("").filter((x) => x != ".")
+    console.log("1: " + path)
+
+    // eliminate final '/'
+    if (path[path.length-1]==="/"){path = path.slice(0, -1)}
+    console.log("2: " + path)
+
+    // get rid of '.'
+    answer = path.split("").filter((x) => x != ".")
+    console.log("3: " + answer)
+
+    // reduce '//' to '/'
     for (let j=0;j<answer.length;j++){
         if (answer[j] == "/" && answer[j+1] == "/"){answer.splice([j],1)
         j--
         }
     }
     answer = answer.join("")
-    // for (let j=answer.length;j>0;j--){
-    //     if (answer[j] == "/"){answer = answer.slice(j)}
-    // }
+    console.log("4: " + answer)
+
+    // once again eliminate final '/'
+    if (answer.length != 1 && answer[answer.length-1]==="/"){answer = answer.slice(0, -1)}
+    console.log("5: " + answer)
+
     return answer
 };
 
-// "/a/./b/../../c/" should be "/c"
-// "/home//foo/" should be "/home/foo"
+/*
+"/a//b////c/d//././/.."
 
+
+Output
+"/"
+Expected
+"/a/b/c"
+Stdout
+1: /..
+2: /..
+3: /
+4: /
+5: /
+
+
+
+
+
+/*
+"/a/../../b/../c//.//"
+"/c/"
