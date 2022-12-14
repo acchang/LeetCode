3229fe8f81680e4d4f8cd30cb97c13f275b927ee
Andrew Chang <acchang4@gmail.com>
2022-12-13 21:35:10 -0500

    hard to replicate in reverse

--- 931-minimum-falling-path.js
+++ 931-minimum-falling-path.js
@@ -3,6 +3,10 @@
  * @return {number}
  */
 
+// https://leetcode.com/problems/minimum-falling-path-sum/solutions/2906130/javascript-iteration-recursion-dynamic-programming-memoization/
+
+// https://www.youtube.com/watch?v=OPwU0D0z1r4
+
 var minFallingPathSum = function(matrix) {
     let n = matrix.length;
 
@@ -29,5 +33,29 @@ var minFallingPathSum = function(matrix) {
         }
     }
     // if the matrix is greater than one square, then take the minSum
+    return matrix.length > 1 ? minSum : matrix[0][0];
+};
+
+// try to replicate from bottom up with reverse:
+
+var minFallingPathSum = function(matrix) {
+    let n = matrix.length;
+
+    let reversed = matrix.reverse()
+    let dp =  new Array(n);
+    for (let i = 0; i < n; i++) {
+        dp[i] = [...reversed [i]]
+    }
+
+    for (let i = 0; i < n-1; i++) {
+        minSum = Infinity;
+        for (let j = 0; j < n; j++) {
+            console.log(i, j)
+            dp[i+1][j] += dp[i][j];
+            if (j > 0) {dp[i+1][j] = Math.min(dp[i+1][j], reversed[i][j] + dp[i+1][j-1])};
+            if (j < n-1) {dp[i+1][j] = Math.min(dp[i][j], matrix[i][j] + dp[i+1][j+1])};
+            minSum = Math.min(minSum, dp[i][j])
+        }
+    }
     return matrix.length > 1 ? minSum : matrix[0][0];
 };
