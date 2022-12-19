60a1d4341849d4d8a374a9cc60eaf1038d48d184
Andrew Chang <acchang4@gmail.com>
2022-12-19 07:11:18 -0500

    book solution

--- 739-daily-temperatures.js
+++ 739-daily-temperatures.js
@@ -8,8 +8,7 @@ var dailyTemperatures = function(temperatures) {
     let output = []
     for (let i=0; i<temperatures.length; i++){
         let add = 1
-        for (j=i+1; j<temperatures.length; j++){
-            console.log(temperatures[i], temperatures[j], add)
+        for (let j=i+1; j<temperatures.length; j++){
             if (temperatures[j]>temperatures[i]){output.push(add); break}
             else if (j===temperatures.length-1){output.push(0); break}
             else {add += 1}
@@ -18,10 +17,10 @@ var dailyTemperatures = function(temperatures) {
     output.push(0)
     return output
 };
+
 // above is too slow, try:
 
 var dailyTemperatures = function(T) {
-    
     const result = [];
     for(let i = 0; i < T.length; i++){
         let nextDay = 0;
@@ -38,15 +37,19 @@ var dailyTemperatures = function(T) {
 
 // refining my solution, not that hard:
 
-var dailyTemperatures = function(temperatures) {
-    let output = []
-    for (let i=0; i<temperatures.length; i++){
-        let wait = 0
-        for (j=i+1; j<temperatures.length; j++){
-            if (temperatures[j]>temperatures[i]){wait = j-i; break}
+var dailyTemperatures = function(T) {
+    const result = [];
+    for (let  i= 0; i < T.length; i++){
+        let nextDay = 0;
+        for (let j=i+1; j<T.length; j++){
+            if (T[j]>T[i]){
+                nextDay = j-i; 
+                break;
+            }
         }
-        output.push(wait)
+        result.push(nextDay)
     }
-    return output
+    return result;
 };
 
+// just forgetting the let in the second loop slowed things immensely
