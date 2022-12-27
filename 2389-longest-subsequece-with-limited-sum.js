e1cc6052b7cf37d7731476cf954b69ca09c85cee
Andrew Chang <acchang4@gmail.com>
2022-12-27 15:00:56 -0700

    reduce

--- 2389-longest-subsequece-with-limited-sum.js
+++ 2389-longest-subsequece-with-limited-sum.js
@@ -4,5 +4,10 @@
  * @return {number[]}
  */
 var answerQueries = function(nums, queries) {
-    // easy?
+    nums = nums.sort((a,b) => a-b)
+    for (let i=0; i<nums.length; i++){
+        while
+        reduce
+        
+    }
 };
