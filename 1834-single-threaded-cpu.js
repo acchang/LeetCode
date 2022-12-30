836b880b882a52bf9cd1d44d7227ed452308662f
Andrew Chang <acchang4@gmail.com>
2022-12-30 11:08:37 -0800

    start

--- 1834-single-threaded-cpu.js
+++ 1834-single-threaded-cpu.js
@@ -7,4 +7,3 @@
 var getOrder = function(tasks) {
     
 };
-
---
+++ 797-all-paths-from-source-to-target.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function(graph) {
+    
+};
+
