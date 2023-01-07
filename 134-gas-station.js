e088544ac6e27910237863c33ffd41b9477d10df
Andrew Chang <acchang4@gmail.com>
2023-01-07 14:54:10 -0500

    done

--- 134-gas-station.js
+++ 134-gas-station.js
@@ -1,15 +1,25 @@
+// https://leetcode.com/problems/gas-station/
+
 /**
  * @param {number[]} gas
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
-
-  1 2 3 4 5
-  3 4 5 1 2 minus
-  7     3 
-build the algo
-if roundtrip is negative, eliminate it
-
-
+const canCompleteCircuit = function(gas, cost) {
+  let n = gas.length;
+    let start = 0;
+    let currentTank = 0;
+    for(let i=0; i<2*n; i++){
+      currentTank += gas[i%n]-cost[i%n];
+        if (currentTank < 0){
+        currentTank = 0;
+        start = i+1;
+      }
+      else{
+        if (i-start+1==n){
+        return start
+      }
+    }
+    }
+return -1
 };
