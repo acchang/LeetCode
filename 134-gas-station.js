4584a46a17829a3d2eada1578a63ae628c638f04
Andrew Chang <acchang4@gmail.com>
2023-01-07 10:22:25 -0500

    laying it out

---
+++ 134-gas-station.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+
+  1 2 3 4 5
+  3 4 5 1 2 minus
+  7     3 
+build the algo
+if roundtrip is negative, eliminate it
+
+
+};
