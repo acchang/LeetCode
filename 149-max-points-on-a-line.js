e4743d1ee3109d615ad653c1a69da502bae7e574
Andrew Chang <acchang4@gmail.com>
2023-01-08 12:42:38 -0500

    34/35 passed

--- 149-max-points-on-a-line.js
+++ 149-max-points-on-a-line.js
@@ -1,11 +1,14 @@
+//https://leetcode.com/problems/max-points-on-a-line/description/
+
 /**
  * @param {number[][]} points
  * @return {number}
  */
 
 var maxPoints = function(points) {
+    if (points.length == 1){return 1}
     let answerMax = 0
-    const slopesObject = {}
+    let slopesObject = {}
     for (let i=0; i<points.length; i++){
         for (let j=0; j<points.length; j++){
             slopesProp = getSlope(points[i], points[j])
@@ -13,9 +16,10 @@ var maxPoints = function(points) {
             if (slopesProp in slopesObject){slopesObject[slopesProp] += 1} else {slopesObject[slopesProp] = 1};
     console.log(j, slopesObject)
         }
-    let latestMax = Math.max(slopesObject[slopesProp])
-    // max = maximum of the slopesObject[slopesProp], if newm
+    let latestMax = Math.max(...(Object.values(slopesObject))) + 1
+    console.log("LM: " + latestMax)
     if (latestMax > answerMax){answerMax = latestMax}
+    slopesObject = {}
     }
     return answerMax
 };
@@ -23,16 +27,3 @@ var maxPoints = function(points) {
 function getSlope(a,b){
     return ((b[0]-a[0])/(b[1]-a[1]))
 }
-
-
-
-
-
-            // get slope for every pair of points
-            // create only one hashmap: if new, slope, hash 1; if not, add 1
-            // when i = j, slope wil be 0
-            // store max recorded value
-            // move on to next in outer loop
-            // if denom is zero, slope is infinity
-            // if they key is present, take assoc value + 1
-            // hold the max as a global maximum, max is number of points with that one point in same line
