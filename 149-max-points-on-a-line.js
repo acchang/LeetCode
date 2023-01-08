c4fdffe99d8fcdce642783000fad06372371b713
Andrew Chang <acchang4@gmail.com>
2023-01-08 05:36:27 -0500

    a start

---
+++ 149-max-points-on-a-line.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/max-points-on-a-line/description/
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    // kind of like linear regression
+    // formula for every 2 points, run on whole, which works best?
+};
+
