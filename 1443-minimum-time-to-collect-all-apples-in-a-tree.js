b88a298940b2cfa934a3f81ef0d16c53b767d638
Andrew Chang <acchang4@gmail.com>
2023-01-11 12:26:23 -0500

    into the algo

--- 1443-minimum-time-to-collect-all-apples-in-a-tree.js
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -19,30 +19,25 @@ var minTime = function(n, edges, hasApple) {
     for (let edge of edges) {
         children[edge[0]].push(edge[1])
         children[edge[1]].push(edge[0])
-        console.log("3: "+ children)
     }
-    console.log("4: "+ children)
 // for each edge, first of two is the index, second is the value
 // then also, second is index, first is the value
+// children is [1,2,0,4,5,0,3,6,2,1,1,2] but that obscures children[0] is [ 1, 2 ] and children[2] is [ 0, 3, 6 ]
 
-    // let res = 0;
-    // let dfs = function (node, parent) {
-    //     let val = false
-    //     for (let child of children[node]) {
-    //         if (child === parent) continue
-    //         res++
-    //         let bol = dfs(child, node)
-    //         if (bol) res++
-    //         else res--
-    //         val = val || bol
-    //     }
-    //     if (hasApple[node]) return true
-    //     return val
-    // }
-    // dfs(0)
-    // return res
+    let res = 0;
+    let dfs = function (node, parent) {
+        let val = false
+        for (let child of children[node]) {
+            if (child === parent) continue
+            res++
+            let bol = dfs(child, node)
+            if (bol) res++
+            else res--
+            val = val || bol
+        }
+        if (hasApple[node]) return true
+        return val
+    }
+    dfs(0)
+    return res
 };
-
-
-
-
