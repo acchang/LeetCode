0997f8625be678cb0130c2e32befaf5bf4e285c5
Andrew Chang <acchang4@gmail.com>
2023-01-11 14:52:17 -0500

    second part

--- 1443-minimum-time-to-collect-all-apples-in-a-tree.js
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -15,7 +15,6 @@ var minTime = function(n, edges, hasApple) {
         children[i] = new Array()
     }
 // each space in array gets an array
-
     for (let edge of edges) {
         children[edge[0]].push(edge[1])
         children[edge[1]].push(edge[0])
@@ -23,18 +22,18 @@ var minTime = function(n, edges, hasApple) {
 // for each edge, first of two is the index, second is the value
 // then also, second is index, first is the value
 // children is [1,2,0,4,5,0,3,6,2,1,1,2] but that obscures children[0] is [ 1, 2 ] and children[2] is [ 0, 3, 6 ]
-
     let res = 0;
-            // console.log(children)
-            //[ [ 1, 2 ], [ 0, 4, 5 ], [ 0, 3, 6 ], [ 2 ], [ 1 ], [ 1 ], [ 2 ] ]
     let dfs = function (node, parent) {
+        console.log("first", node, parent)
         let val = false
+        //[ [ 1, 2 ], [ 0, 4, 5 ], [ 0, 3, 6 ], [ 2 ], [ 1 ], [ 1 ], [ 2 ] ]
         for (let child of children[node]) {
-            // for 1 or 2 of 0
-            // round 2: for [0,4,5] of 1
+            // for 1 or 2 of node 0
+            // round 2: for [0,4,5] of node 1, parent is still 0
+            // round 3: for [1] of 4, 1 == parent
             console.log("test", children[node], node)
-            if (child === parent) {console.log("no"); continue}
-            // don't proceed if it's the same
+            if (child === parent) {console.log("no", child, parent); continue}
+            // jump to after dfs if the node connects to the parent
             console.log("yes")
             res++
             // add one to result
@@ -43,13 +42,14 @@ var minTime = function(n, edges, hasApple) {
             // round 2 is 4, 0, 2
             let bol = dfs(child, node)
             // run dfs again of 1,0, where 1 is now the node and parent is zero
-            /// round 2: dfs again, 4 is node, parent is 1 
-
-
+            /// round 2: dfs again, child 4 is now node, parent is now prev node or 1
             console.log("bol", bol, child, node)
             if (bol) res++
+            // node connects to parent so add route, if not, delete it
             else res--
             val = val || bol
+            // this is a false or true
+            console.log("val:", val)
         }
         if (hasApple[node]) return true
         return val
@@ -57,6 +57,9 @@ var minTime = function(n, edges, hasApple) {
     dfs(0)
     return res
 };
+
+
+
 
 
 
