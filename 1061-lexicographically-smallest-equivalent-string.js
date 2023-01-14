71861bf2913bddac00436413fa6f5db6db3f4950
Andrew Chang <acchang4@gmail.com>
2023-01-14 06:18:16 -0500

    barely understand prob

---
+++ 1061-lexicographically-smallest-equivalent-string.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/lexicographically-smallest-equivalent-string/description/
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} baseStr
+ * @return {string}
+ */
+var smallestEquivalentString = function(s1, s2, baseStr) {
+    // abc
+    // cde
+    // eed
+    // aab
+
+    //parker
+    //morris
+    
+    //parser
+
+};
