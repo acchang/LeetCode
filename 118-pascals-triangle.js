e4e8f44a4ab4f949d5e90f6bda078fc075e3e8a6
Andrew Chang <acchang4@gmail.com>
2023-01-14 06:36:32 -0500

    start

---
+++ 118-pascals-triangle.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/pascals-triangle/
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let answer = [[1]]
+    let subAnswer = []
+    while (numRows) {
+        subAnswer.push(an)
+    }
+};
+
