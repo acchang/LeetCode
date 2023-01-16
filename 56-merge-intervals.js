e8126be04dfd977e30bd17942c69d99620845edd
Andrew Chang <acchang4@gmail.com>
2023-01-16 08:31:53 -0500

    done

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -12,17 +12,13 @@
 
 var merge = function(intervals) {
     intervals = intervals.sort((a,b) => a[0]-b[0])
-    for (let i=1; i<intervals.length; i++){
-        if (intervals[i][0] <= intervals[i-1][1] && intervals[i][1] <= intervals[i-1][1]) {
-            intervals.splice(i, 1);
-            continue
-            };
-        if (intervals[i][0] <= intervals[i-1][1]){
-            intervals[i-1][1] = intervals[i][1];
-            intervals.splice(i, 1) 
-        }
+    let answer = []
+    for (let i=0; i<intervals.length; i++){
+        const last = answer.at(-1);
+        if (!answer.length || intervals[i][0] > last[1]){answer.push(intervals[i])}
+        else (last[1] =  Math.max(last[1], intervals[i][1]))
     }
-    return intervals
+    return answer
 };
 
 
