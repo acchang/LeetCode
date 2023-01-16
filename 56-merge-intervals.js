e1349af22d8ad3e5833d8aaa20330da3d6b3b837
Andrew Chang <acchang4@gmail.com>
2023-01-16 08:24:41 -0500

    one solution

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -3,7 +3,12 @@
  * @return {number[][]}
  */
 // [[1,4],[4,5]] OK
-// [[1,4],[2,3]] NO
+// [[1,4],[2,3]] OK
+// fails on 
+// [[1,4],[0,2],[3,5]]
+// [[0,4],[3,5]]
+// Expected
+// [[0,5]]
 
 var merge = function(intervals) {
     intervals = intervals.sort((a,b) => a[0]-b[0])
@@ -18,4 +23,24 @@ var merge = function(intervals) {
         }
     }
     return intervals
+};
+
+
+// working through this solution: https://leetcode.com/problems/merge-intervals/solutions/2105757/
+
+var merge = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0]);
+    const results = [];
+    // use the results to compare
+    for(let interval of intervals) {
+        const prev = results.at(-1);
+        if (!results.length || prev[1] < interval[0]) {
+    // if the holder has nothing or the end of what's in there is less than the new one, just put a new one in
+            results.push(interval);
+        } else {
+            prev[1] = Math.max(prev[1], interval[1])
+    // otherwise if the end of what's in there is greater, then the end of what's in there is the greater of what's in there or the new one
+        }
+    }
+    return results;
 };
