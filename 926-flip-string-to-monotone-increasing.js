6319b37c6a91c04a3d8fe0a90e147f20a99cc382
Andrew Chang <acchang4@gmail.com>
2023-01-17 08:46:22 -0500

    experimentation

--- 926-flip-string-to-monotone-increasing.js
+++ 926-flip-string-to-monotone-increasing.js
@@ -4,17 +4,10 @@
  * @param {string} s
  * @return {number}
  */
-var minFlipsMonoIncr = function(s) {
-    // min flips to go all 1, all 0 or 01
-    // look at zeroes from the start or ones from the end
-    // find the turning point
-    // from the end, how many 0 to become 1 
-    // from the front, how many 1 to become 0
-    // do some kind of recursion
-};
-
-
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var minFlipsMonoIncr = function(s) {
     const n = s.length;
     let zeros = 0;
@@ -22,20 +15,28 @@ var minFlipsMonoIncr = function(s) {
     for (let i = n - 1; i >= 0; i--) {
         if (s.charAt(i) === "0") zeros++;
     }
-    
-    let minFlips = Number.MAX_SAFE_INTEGER;
+    // count the zeros from the end
+    console.log(zeros)
+    let minFlips = Infinity;
     let ones = 0;
     
     for (let i = 0; i < n; i++) {
-        const digit = s.charAt(i);
-
         minFlips = Math.min(minFlips, ones + zeros);
+        console.log("loop", minFlips, ones + zeros)
+        // first go is 0 (ones) + zeros from the end
+        // 00011000
+        // 6, 2
+        // 2 is the answer, zeroes go to 0
+        // 010110
+        // z=3, look at minFlips
 
-        if (digit === "1") ones++;
+
+        if (s.charAt(i) === "1") ones++;
         else zeros--;
     }
    
+   console.log("end1", minFlips, ones, zeros)
     minFlips = Math.min(minFlips, ones + zeros);
-    
+    console.log("end", minFlips)
     return minFlips;
 };
