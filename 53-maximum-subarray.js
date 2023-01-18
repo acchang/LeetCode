821f67a21622ca09b8054b2e8baa2b9d6a45d066
Andrew Chang <acchang4@gmail.com>
2023-01-18 14:28:01 -0500

    done

--- 53-maximum-subarray.js
+++ 53-maximum-subarray.js
@@ -1,14 +1,15 @@
-// https://www.youtube.com/watch?v=86CQq3pKSUw
+// Kadane's Algo: https://www.youtube.com/watch?v=86CQq3pKSUw
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let curMax = 0
-    let globalMax = 0
+    let curMax = -Number.MAX_VALUE
+    let globalMax = -Number.MAX_VALUE
     for (let num of nums){
         curMax = Math.max(num, curMax+num)
+        // the max is either a chain of everything before it, or just the current.
         if (curMax>globalMax){globalMax=curMax}
         }
     return globalMax
