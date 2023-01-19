707c7f11da9c49a56a55b6b8945205e755be5797
Andrew Chang <acchang4@gmail.com>
2023-01-18 19:51:56 -0500

    solved

--- 918-maximum-sum-circular-subarray.js
+++ 918-maximum-sum-circular-subarray.js
@@ -1,16 +1,29 @@
 // https://leetcode.com/problems/maximum-sum-circular-subarray/
+// from: https://www.youtube.com/watch?v=fxT9KjakYPM&t=569s
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubarraySumCircular = function(nums) {
-    let doubled = [...nums, ...nums];
-    let curMax = -Number.MAX_VALUE
-    let globalMax = -Number.MAX_VALUE
-    for (let i=0; i<doubled.length; i++){
-        curMax = Math.max(doubled[i], curMax+doubled[i])
-        if (curMax>globalMax){globalMax=curMax}
-        }
-    return globalMax
+    let globalMax = nums[0]
+    let globalMin = nums[0]
+    let curMax = 0
+    let curMin = 0
+    let total = 0
+    for (num of nums){
+        curMax = Math.max(curMax+num, num);
+        curMin = Math.min(curMin+num, num);
+        total += num;
+        globalMax = Math.max(globalMax,curMax);
+        globalMin = Math.min(globalMin,curMin);
+        // go through with Kadane's algorithm and get the total and the global MaxSum and global MinSum.
+    }
+    if (globalMax > 0) {
+        // if the globalMax is not negative subtract from total, otherwise just use globalMax
+        return Math.max(globalMax, total - globalMin)
+    }
+      else {return globalMax};
 };
+
+
