360e5218b4f152e86bf11b6d7a0de736a2875020
Andrew Chang <acchang4@gmail.com>
2023-01-22 08:21:28 -0500

    hold for now

--- 131-palindrome-partitioning.js
+++ 131-palindrome-partitioning.js
@@ -2,6 +2,33 @@
  * @param {string} s
  * @return {string[][]}
  */
+
 var partition = function(s) {
-    
+    let word 
+    while (word.length > 0){
+        word = s.split(at half mark)
+    }
+// EVERY substring is a palindrome
+// uses backtracking, try to solve previous
+
+// recursively split s and then put through helper function
+// full, then each half but if odd then both have to be split
+// until word length > 0
+// The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
+
+}
+
+function isPalindrome(word) {
+for (let i=0; i<Math.floor(word.length/2); i++){
+    if (word[i] === word[length-1-i]){
+        continue
+    } else break
+    }
+return word
+}
+
+    // divide in half and keep doing so, and put in answer if correct
+    // if odd then ignore the middle character, or take for end, and match
+
+
 };
