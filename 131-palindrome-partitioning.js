d3fb40577a3a6c837b70b551d7b826c3571a3b5f
Andrew Chang <acchang4@gmail.com>
2023-01-23 10:48:13 -0500

    notes

--- 131-palindrome-partitioning.js
+++ 131-palindrome-partitioning.js
@@ -1,34 +1,32 @@
+// Jan 21 2023: https://leetcode.com/problems/palindrome-partitioning/
+
 /**
  * @param {string} s
  * @return {string[][]}
  */
 
 var partition = function(s) {
-    let word 
-    while (word.length > 0){
-        word = s.split(at half mark)
+    let answer = []
+    let part = []
+    if (i === s.length){
+        answer.push(part)
     }
-// EVERY substring is a palindrome
-// uses backtracking, try to solve previous
-
-// recursively split s and then put through helper function
-// full, then each half but if odd then both have to be split
-// until word length > 0
-// The substring() method extracts characters, between two indices (positions), from a string, and returns the substring.
-
+    
+    return answer
 }
 
-function isPalindrome(word) {
+var isPalindrome = function(word) {
 for (let i=0; i<Math.floor(word.length/2); i++){
-    if (word[i] === word[length-1-i]){
+    if (word[i] === word[word.length-1-i]){
         continue
     } else break
     }
 return word
 }
 
-    // divide in half and keep doing so, and put in answer if correct
+
+// EVERY substring is a palindrome
+// uses backtracking, try to solve previous
+
+// divide in half and keep doing so, and put in answer if correct
     // if odd then ignore the middle character, or take for end, and match
-
-
-};
