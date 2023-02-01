8175b3c2bed5dc0cf3f7603e8556ed51c53474ad
Andrew Chang <acchang4@gmail.com>
2023-02-01 08:30:01 -0500

    initial test

--- 1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings.js
@@ -6,16 +6,18 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    let short, long, testPhrase
+    let short, long, testPhrase, testSeg
     if (str1.length < str2.length) {short = str1, long = str2}
     else {short = str2, long = str1};
 
     for (let i=0; i<short.length; i++){
         if (long.length % short.length-i === 0){
-            testPhrase = short.repeat(long.length/short.length)
-            console.log(long.length, short.length-i, testPhrase)
-        }
-        if (testPhrase === long){return short.slice(-i)} else {console.log(testPhrase, 'no')}
+            testSeg = short.slice(-i)
+            testPhrase = testSeg.repeat(long.length/(short.length-i))
+            if (testPhrase === long){return testSeg}
+        };
     };
-}
+    return ""
+};
+
 
