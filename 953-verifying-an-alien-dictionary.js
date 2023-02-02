7316746eaec56de37f34f4020953d739cbde57c3
Andrew Chang <acchang4@gmail.com>
2023-02-02 04:23:04 -0500

    a plan

---
+++ 953-verifying-an-alien-dictionary.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    // order of words follow order
+    // create a hashmap of order
+    // use hashmap to translate words
+    // sort translated words, if change, then false
+};
+
