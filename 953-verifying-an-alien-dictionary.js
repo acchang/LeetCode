24bf779f842863d336ec2be8f799c4c9d269fcee
Andrew Chang <acchang4@gmail.com>
2023-02-02 05:04:35 -0500

    done

--- 953-verifying-an-alien-dictionary.js
+++ 953-verifying-an-alien-dictionary.js
@@ -1,9 +1,17 @@
+// https://leetcode.com/problems/verifying-an-alien-dictionary/
+
 /**
  * @param {string[]} words
  * @param {string} order
  * @return {boolean}
  */
 var isAlienSorted = function(words, order) {
+    let alpha = "abcdefghijklmnopqrstuvwxyz"
+    let translated = {}
+    for (let i=0; i<order.length; i++){
+        translated[alpha[i]]=order[i]
+    }
+    console.log(translated)
     // order of words follow order
     // create a hashmap of order
     // use hashmap to translate words
