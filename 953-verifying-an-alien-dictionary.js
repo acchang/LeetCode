3552f6ea6b1c1ad8566f82c0ee2a30a833d3fb32
Andrew Chang <acchang4@gmail.com>
2023-02-03 04:42:35 -0500

    start

--- 953-verifying-an-alien-dictionary.js
+++ 953-verifying-an-alien-dictionary.js
@@ -7,14 +7,44 @@
  */
 var isAlienSorted = function(words, order) {
     let alpha = "abcdefghijklmnopqrstuvwxyz"
-    let translated = {}
+    let dictionary = {}
+    let translatedWords = []
     for (let i=0; i<order.length; i++){
-        translated[alpha[i]]=order[i]
+        dictionary[order[i]]=alpha[i]
     }
-    console.log(translated)
-    // order of words follow order
-    // create a hashmap of order
-    // use hashmap to translate words
-    // sort translated words, if change, then false
+    for (word of words) {
+        let translatedWord = "";
+        for (let j=0; j<word.length; j++){
+            translatedWord = translatedWord.concat(dictionary[word[j]])
+        }
+        translatedWords.push(translatedWord)
+    }
+    let sortedTranslatedWords = [...translatedWords].sort();
+    return sortedTranslatedWords.every((val, index) => val === translatedWords[index])
 };
+
+
+
+// improved:
+// from: https://www.youtube.com/watch?v=DJHPBFmDtTw
+
+var isAlienSorted = function(words, order) {
+    let key= {}
+    for (let i=0; i<order.length; i++){
+        key[order[i]]=i
+    }
+    for (let i=0; i<words.length-1; i++) {
+        for (let j=0; j<words[i].length; j++){
+            // if one word is before another, the next cannot come before.
+            if (j>=words[i+1].length){return false}
+            // if the latter word is longer (everything else same), false
+            if (words[i][j] === words[i+1][j]){continue}
+            // if letter of first word is same as next word, continue
+            if (key[words[i][j]]<key[words[i+1][j]]){break} else {return false}
+            // the letter of first word should be before the letter of second word, if not return false
+            // you break because once the letter of first word is before letter of second, you examine next word. continue would examine every letter.
+        }
+    }
+    return true
+}
 
