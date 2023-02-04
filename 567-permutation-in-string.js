31d983dea2c1fd6765185a7375b39939013f72db
Andrew Chang <acchang4@gmail.com>
2023-02-04 05:07:43 -0500

    ideas

---
+++ 567-permutation-in-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    // does s2 contain a form of s1?
+    // all forms of s1 test into s2 is brute force
+    // kadanes? is 1 in? combo of 2?
+    //  check all possible substrings of length n1 in string s2
+    // compare the frequency of characters in each of these substrings to the frequency of characters in string s1
+};
+
