7bd9f4b915bd47e64d25d0f76abc8f12ce52ba75
Andrew Chang <acchang4@gmail.com>
2023-02-05 06:23:32 -0500

    an issue with every

--- 438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string.js
@@ -6,16 +6,17 @@
 var findAnagrams = function(s, p) {
     let answer = []
     let pMap = {}
-    for let (i=0;i<p.length;i++){
+    for (let i=0;i<p.length;i++){
         if (!pMap[p[i]]){pMap[p[i]]=1} else {pMap[p[i]]+1}
     }
-    for (let i=0; i<s.length-p.length; i++){
+
+    for (let i=0; i<s.length-p.length+1; i++){
         let chunk = s.substring(i,i+p.length)
-        let temp = [...pMap]
+        let temp = {...pMap}
         for (let j=0; j<chunk.length; j++){
-            pMap[chunk[i]]--
+            if(temp[chunk[j]]){temp[chunk[j]]-- }
         }
-        if (object.keys(temp)).every(x => x=0)){answer.push[i]}
+        if (Object.keys(temp)).every(x => x==0)){answer.push[i]}
     }
     return answer
 };
