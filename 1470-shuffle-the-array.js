f2cc503f32553915d3bc35f63f5088f2538c4faf
Andrew Chang <acchang4@gmail.com>
2023-02-05 21:44:45 -0500

    first try done

---
+++ 1470-shuffle-the-array.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/shuffle-the-array/
+
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let answer = [];
+    for (let i=0; i<n; i++){
+        answer.push(nums[i],nums[i+n])
+    }
+    return answer
+};
+
