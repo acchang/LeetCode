f4e4ed7880968212396392b55a84f1c77aa67fef
Andrew Chang <acchang4@gmail.com>
2023-02-06 21:21:32 -0500

    try

---
+++ 904-fruit-into-baskets.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} fruits
+ * @return {number}
+ */
+var totalFruit = function(fruits) {
+    // two fruits
+    // in the array, which two combinations occur most?
+    // sliding window with look ahead 
+    // if next 2 
+
+};
