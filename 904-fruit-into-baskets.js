629c8561d57b704bbb10a39e2eea25669b80fdce
Andrew Chang <acchang4@gmail.com>
2023-02-07 08:37:46 -0500

    laying out ideas

--- 904-fruit-into-baskets.js
+++ 904-fruit-into-baskets.js
@@ -3,9 +3,23 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
+    let l = 0;
+    const surveyed = new Map();
+    let max = 0;
+
+    for(let right = 0; right<fruits; right ++){
+        let rightChar = fruits[right]
+        if (!surveyed.get(rightChar)){surveyed.set(rightChar, 1)};
+        else {surveyed.set(rightChar, surveyed.get(rightChar)+1)};
+
+
+        
+    }
+
     // two fruits
     // in the array, which two combinations occur most?
     // sliding window with look ahead 
     // if next 2 
+// https://medium.com/theleanprogrammer/js-longest-substring-with-k-distinct-characters-sliding-window-technique-7585a0d34ee4
 
 };
