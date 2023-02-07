87b183f58ef08af56bb29a04e6036536fb5e0ed5
Andrew Chang <acchang4@gmail.com>
2023-02-07 10:21:53 -0500

    done, 80%run,56% mem

--- 904-fruit-into-baskets.js
+++ 904-fruit-into-baskets.js
@@ -3,23 +3,24 @@
  * @return {number}
  */
 var totalFruit = function(fruits) {
-    let l = 0;
+    let sectionStart = 0;
     const surveyed = new Map();
     let max = 0;
 
-    for(let right = 0; right<fruits; right ++){
-        let rightChar = fruits[right]
-        if (!surveyed.get(rightChar)){surveyed.set(rightChar, 1)};
-        else {surveyed.set(rightChar, surveyed.get(rightChar)+1)};
+    for(let sectionEnd = 0; sectionEnd<fruits.length; sectionEnd ++){
+        let rightChar = fruits[sectionEnd];
+        if (!surveyed.get(rightChar)){surveyed.set(rightChar, 1)}
+        else surveyed.set(rightChar, surveyed.get(rightChar)+1)
 
-
-        
-    }
-
-    // two fruits
-    // in the array, which two combinations occur most?
-    // sliding window with look ahead 
-    // if next 2 
-// https://medium.com/theleanprogrammer/js-longest-substring-with-k-distinct-characters-sliding-window-technique-7585a0d34ee4
-
+        while (surveyed.size > 2){
+            let leftChar = fruits[sectionStart];
+            if (surveyed.get(leftChar)>1) {surveyed.set(leftChar, surveyed.get(leftChar)-1)} else surveyed.delete(leftChar);
+            sectionStart++;
+        }
+    
+        max = Math.max(max, sectionEnd-sectionStart+1);
+    };
+    return max
 };
+
+// https://medium.com/theleanprogrammer/js-longest-substring-with-k-distinct-characters-sliding-window-technique-7585a0d34ee4
