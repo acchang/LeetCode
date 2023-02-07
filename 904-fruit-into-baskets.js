c491e73716fb29a37d0016e9fe898fc1b5e5b3c6
Andrew Chang <acchang4@gmail.com>
2023-02-07 10:42:51 -0500

    refactored, now 89/55

--- 904-fruit-into-baskets.js
+++ 904-fruit-into-baskets.js
@@ -2,6 +2,35 @@
  * @param {number[]} fruits
  * @return {number}
  */
+
+
+// changed while to if when too long, boosted from 80/56 to 89/55.
+
+var totalFruit = function(fruits) {
+    let sectionStart = 0;
+    const surveyed = new Map();
+    let max = 0;
+
+    for(let sectionEnd = 0; sectionEnd<fruits.length; sectionEnd ++){
+        let rightChar = fruits[sectionEnd];
+        if (!surveyed.get(rightChar)){surveyed.set(rightChar, 1)}
+        else surveyed.set(rightChar, surveyed.get(rightChar)+1)
+
+        if (surveyed.size > 2){
+            let leftChar = fruits[sectionStart];
+            if (surveyed.get(leftChar)>1) {surveyed.set(leftChar, surveyed.get(leftChar)-1)} else surveyed.delete(leftChar);
+            sectionStart++;
+        }
+    
+        max = Math.max(max, sectionEnd-sectionStart+1);
+    };
+    return max
+};
+
+
+
+// inspired by #340: https://www.youtube.com/watch?v=lz8lyKKZYPY
+
 var totalFruit = function(fruits) {
     let sectionStart = 0;
     const surveyed = new Map();
