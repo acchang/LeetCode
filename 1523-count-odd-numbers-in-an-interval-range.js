0d1d0002e7c16ebd80846e38348fd330c479e7fb
Andrew Chang <acchang4@gmail.com>
2023-02-12 19:40:43 -0500

    88%run,62%mem

--- 1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range.js
@@ -3,9 +3,30 @@
  * @param {number} high
  * @return {number}
  */
+
+//88% run, 62% memory
+
 var countOdds = function(low, high) {
     let output = 0
     if (low%2!=0 && high%2!=0){output = ((high-low)/2) +1} else 
     {output = (high-low)/2}
     return output
 };
+
+
+// alt method: check if low is odd; if not, we can just increment it
+// https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/solutions/2957338/count-odd-numbers-in-an-interval-range/?orderBy=most_relevant
+
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    if (low%2==0){low = low+1}
+    return low > high? 0 : Math.floor((high-low)/2) +1 
+};
+
+// only 74% run, 72% memory
+
+
