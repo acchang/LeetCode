7c69737fb8dc1763647bc0d5673741d82a43e734
Andrew Chang <acchang4@gmail.com>
2023-02-12 19:28:15 -0500

    passed tests

--- 1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range.js
@@ -4,6 +4,8 @@
  * @return {number}
  */
 var countOdds = function(low, high) {
-    
+    let output = 0
+    if (low%2!=0 && high%2!=0){output = ((high-low)/2) +1} else 
+    {output = (high-low)/2}
+    return output
 };
-
