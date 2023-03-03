f6b8aff88228034c08042384f73029983fb3e13b
Andrew Chang <acchang4@gmail.com>
2023-03-02 22:01:14 -0500

    79run,89memory

--- 28-find-the-first-occurrence-in-a-string.js
+++ 28-find-the-first-occurrence-in-a-string.js
@@ -6,11 +6,13 @@ https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    for (let i=0; i<haystack.length-needle.length-1; i++){
-        if (haystack.slice(i,needle.length) === needle){
+    for (let i=0; i<haystack.length; i++){
+        // console.log(i, haystack.slice(i,i+needle.length), needle)
+        if (haystack.slice(i,i+needle.length) === needle){
             return i
         }
     }
     return -1
 };
+
 
