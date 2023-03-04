595d2a40477ffecaebf728400d4e23ff5b6a8fd8
Andrew Chang <acchang4@gmail.com>
2023-03-03 20:57:31 -0500

    start

---
+++ 2444-count-subarrays-with-fixed-bounds.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} minK
+ * @param {number} maxK
+ * @return {number}
+ */
+var countSubarrays = function(nums, minK, maxK) {
+    // cycle through all possible arrays and make sure the min and max match
+};
