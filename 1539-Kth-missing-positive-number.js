382b16f588d1ecf531fb360b7cda3462676fc948
Andrew Chang <acchang4@gmail.com>
2023-03-05 20:49:10 -0500

    slightly optimized

--- 1539-Kth-missing-positive-number.js
+++ 1539-Kth-missing-positive-number.js
@@ -1,5 +1,5 @@
 // https://leetcode.com/problems/kth-missing-positive-number/
-
+// my solution is not too off the top ones.
 
 /**
  * @param {number[]} arr
@@ -13,5 +13,20 @@ var findKthPositive = function(arr, k) {
             if (counter == k){return i}
             counter++
             }
+        }
+};
+
+// slightly more optimized, it will never start at zero because that is not a positive number:
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let counter = 0
+    for (let i=1;; i++){
+        if (!arr.includes(i)){counter++}
+        if (counter == k){return i}
         }
 };
