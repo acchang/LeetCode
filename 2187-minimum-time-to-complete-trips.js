ca4b1c650571874e3dca638ea7e0ca6cbe5c9567
Andrew Chang <acchang4@gmail.com>
2023-03-06 22:27:17 -0500

    a plan

---
+++ 2187-minimum-time-to-complete-trips.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} time
+ * @param {number} totalTrips
+ * @return {number}
+ */
+var minimumTime = function(time, totalTrips) {
+    take the time array and make each the denominator (use map)
+    build a loop and each time, add to itself, take floor of each, lay out a counter
+    when the sum is greater than totalTrips, that is your answer.
+};
