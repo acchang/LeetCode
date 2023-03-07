4b68d7612db5a9d285c22b50cb08ca3ba8d350a2
Andrew Chang <acchang4@gmail.com>
2023-03-07 11:17:33 -0500

    interpreted answer

--- 2187-minimum-time-to-complete-trips.js
+++ 2187-minimum-time-to-complete-trips.js
@@ -3,14 +3,40 @@
  * @param {number} totalTrips
  * @return {number}
  */
-var minimumTime = function(time, totalTrips) {
-    let currentTrips = 0
-    let currentTime = 0
 
-    while (currentTrips < totalTrips){
-        currentTime ++
-        currentTrips = time.reduce((acc,cV) => acc + Math.floor(currentTime*(1/cV)), 0)
-        }
-        // needs initial value, if not, accumulates to first in array
-        return currentTime
+/**
+ * @param {number[]} time
+ * @param {number} totalTrips
+ * @return {number}
+ */
+var minimumTime = function(time, totalTrips) {
+    function getOutcome(t) {
+        return time.reduce((acc, cur) => acc + Math.floor(t / cur), 0)
+    }
+    const minTime = Math.min(...time)
+    // minTime is the minimum of the array time (or the fastest)
+
+    let low = 1, high = totalTrips * minTime
+    // high boundary is fastest * trips, low is 1
+
+    // find earliest high such that getOutcome(high) >= totalTrips --- ??
+
+    while (low < high) {
+    // while the low is less than highest possible
+
+        const mid = Math.floor((low + high) / 2)
+// take the mid point, which is (1 + 5)/2 = 3
+
+        const outcome = getOutcome(mid)
+// does time of 3 get you more than total trips
+
+        if (outcome >= totalTrips)  high = mid
+// if not, then narrow it one way
+
+        else low = mid + 1
+// then the other
+    }
+    return high
 };
+
+
