258474e46292432f26c4f0803ab66d17e785f2a8
Andrew Chang <acchang4@gmail.com>
2023-03-07 21:14:16 -0500

    a start

---
+++ 875-koko-eating-bananas.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    // another binary search problem settling on output
+    // one end, rate of quickest eat is max of piles
+    // other is 1, which will not fulfill eating all
+    // find the number that fulfills less than max of piles
+};
