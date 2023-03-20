cbfc04a18c245e04c78c5ad1e0dcd315175477bb
Andrew Chang <acchang4@gmail.com>
2023-03-20 06:18:46 -0400

    pretty basic

--- 605-can-place-flowers.js
+++ 605-can-place-flowers.js
@@ -4,12 +4,28 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function(flowerbed, n) {
+    if (flowerbed[0]===0 && flowerbed.length===1){
+        return true
+    }
+
+    if (flowerbed[0]===0 && flowerbed[1]===0){
+        flowerbed.splice(0, 1, 1);
+                n--
+    }
+    
     for (let i=1; i<flowerbed.length; i++){
         if (flowerbed[i-1] === 0 && 
             flowerbed[i] === 0 &&
             flowerbed[i+1] === 0) {
+                flowerbed.splice(i, 1, 1);
                 n--
             }
     }
+
+    if (flowerbed[flowerbed.length-1]===0 && flowerbed[flowerbed.length-2]===0){
+        flowerbed.splice(flowerbed.length-1, 1, 1);
+                n--
+    }
+
     if (n<1){return true} else {return false}
 };
