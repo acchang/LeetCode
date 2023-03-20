b07fdc53e8ddd9b33f6dd5945b3b5feab273c153
Andrew Chang <acchang4@gmail.com>
2023-03-20 06:20:55 -0400

    optimized

--- 605-can-place-flowers.js
+++ 605-can-place-flowers.js
@@ -29,3 +29,20 @@ var canPlaceFlowers = function(flowerbed, n) {
 
     if (n<1){return true} else {return false}
 };
+
+
+// better, using nots, start at zero.
+
+var canPlaceFlowers = function(flowerbed, n) {
+    for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+      if (
+        flowerbed[i] === 0 &&
+        flowerbed[i - 1] !== 1 &&
+        flowerbed[i + 1] !== 1
+      ) {
+        n--;
+        i++;
+      }
+    }
+    return n === 0;
+  };
