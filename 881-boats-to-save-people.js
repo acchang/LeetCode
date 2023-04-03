a4d09d9b3ee34d665201457e94af2cfebfbd2507
Andrew Chang <acchang4@gmail.com>
2023-04-03 15:58:52 -0400

    optimize attempt

--- 881-boats-to-save-people.js
+++ 881-boats-to-save-people.js
@@ -6,10 +6,8 @@
 var numRescueBoats = function(people, limit) {
     people.sort((a,b) => a - b);
     let counter = 0
-    while (people.length > 0){
-        let low = 0
-        let high = people.length-1
-        if (people[low] + people[high] <= limit){people.pop(), people.shift(), counter++} else {people.pop(), counter++}
+    while (people[0]){
+        if (people[0] + people[people.length-1] <= limit){people.pop(), people.shift(), counter++} else {people.pop(), counter++}
     }
     return counter
 };
