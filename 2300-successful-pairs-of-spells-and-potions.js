6795b94f8b9e578ae0830c128f4b6681ac08baae
Andrew Chang <acchang4@gmail.com>
2023-04-03 15:33:54 -0400

    good. done

--- 2300-successful-pairs-of-spells-and-potions.js
+++ 2300-successful-pairs-of-spells-and-potions.js
@@ -1,3 +1,8 @@
+// https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/
+// * forgot to sort potions when doing a binary search, I need to do this so binary search works
+// * no need for a mid = target condition because not looking for a number but or max or min
+// * subtract low since that's the highest number when it violates the `while (low <= high)` condition.
+
 /**
  * @param {number[]} spells
  * @param {number[]} potions
@@ -5,15 +10,20 @@
  * @return {number[]}
  */
 var successfulPairs = function(spells, potions, success) {
+    potions.sort((a,b) => a - b);
+    
     let answer = []
     for (element of spells){
-        let left = potions[0]
-        let right = potions[length-1]
-            while right > left {
-            let mid = Math.floor(right-left/2)
-            if (mid * element === success){answer.push(potions[length]-mid)}
-            // binary search until found
+        let low = 0
+        let high = potions.length-1
+            while (low <= high) {
+            let mid = Math.floor((high+low)/2)
+            let product = element * potions[mid]
+            if (product >= success){high = mid - 1} else {low = mid + 1}
             }
+        {answer.push(potions.length-low)}
    } 
 return answer
 };
+
+
