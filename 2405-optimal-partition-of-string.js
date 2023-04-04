a947196c70c2881e0b10e5e108ccfc3cc03cc2a5
Andrew Chang <acchang4@gmail.com>
2023-04-04 11:59:13 -0400

    slight improvement

--- 2405-optimal-partition-of-string.js
+++ 2405-optimal-partition-of-string.js
@@ -2,6 +2,10 @@
  * @param {string} s
  * @return {number}
  */
+
+// https://leetcode.com/problems/optimal-partition-of-string/submissions/927940762/
+// pretty slow, runtime 22%; memory 46%
+
 var partitionString = function(s) {
     let counter = 0
     let left = 0
@@ -23,3 +27,20 @@ function checkUnique(str) {
 	} 
 	return str.length === list.size; 
 }; 
+
+// faster: only 37% runtime now, still 47% memory
+// starts with a set of uniques, the moment the new character is in the uniques, make a new set and counter adds 1
+// need to know set methods has and add better
+// c has to go into the new set
+
+var partitionString = function(s) {
+    let set = new Set()
+    let counter = 1
+    for (c of s){
+        if (!set.has(c)){set.add(c)} else {
+            counter++
+            set = new Set(c)
+        }
+}
+return counter
+};
