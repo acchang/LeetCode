47b93cb5083005f5fa204cc3c1ea78d5447e8d49
Andrew Chang <acchang4@gmail.com>
2023-04-05 13:43:28 -0400

    start

---
+++ 2349-minimize-maximum-of-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimizeArrayValue = function(nums) {
+    Perform a binary search over the minimum value that can be achieved for the maximum number of the array.
+1, 2, 3
+i = 1, etc
+
+    In each binary search iteration, iterate through the array backwards, greedily decreasing the current element until it is within the limit.
+
+};
+
+3716
+123
