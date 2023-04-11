647fffca0437f851bcd45dac144f39ca014b4d0d
Andrew Chang <acchang4@gmail.com>
2023-04-11 06:14:53 -0700

    solution

--- 2390-removing-stars-from-a-string.js
+++ 2390-removing-stars-from-a-string.js
@@ -1,7 +1,26 @@
+// https://leetcode.com/problems/removing-stars-from-a-string/description/
+
 /**
  * @param {string} s
  * @return {string}
  */
+
+
+var removeStars = function(s) {
+    const stackArr = [];
+    for(let i=0;i<s.length;i++){
+        if(s[i] === "*" && stackArr.length){
+            stackArr.pop();
+        } else {
+            stackArr.push(s[i]);
+        }
+    }
+    return stackArr.join("");
+};
+
+
+
+// too slow:
 var removeStars = function(s) {
     s = s.split("")
     while (s.includes("*")){
