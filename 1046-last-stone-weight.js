169a8855ad267061af0c9deab7d541ce431501e8
Andrew Chang <acchang4@gmail.com>
2023-04-24 07:31:38 -0400

    done

---
+++ 1046-last-stone-weight.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/last-stone-weight/
+
+
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while (stones.length > 1){
+        stones.sort( (a,b) => b-a);
+        if (stones[0]==stones[1]){stones.splice(0,2)}
+        else if (stones[0]>stones[1]){stones.splice(0,2, stones[0]-stones[1])}
+    }
+    return stones
+};
+
