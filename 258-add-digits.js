f1874ac8873e21fed5420a448023cdadb1e923bd
Andrew Chang <acchang4@gmail.com>
2023-04-26 09:23:07 -0400

    learn digital root

--- 258-add-digits.js
+++ 258-add-digits.js
@@ -17,3 +17,14 @@ var addDigits = function(num) {
     }
     return answer
 };
+
+// better faster solution:
+
+var addDigits = function(num) {
+    if (isNaN(num) || num === 0) return 0;
+    if (num < 10) return num;
+    return num % 9 === 0 ? 9 : num % 9;
+};
+
+// if num is a multiple of 9, it becomes 9, ie 18 or 27 or 99.
+// otherwise return the modulo of 9, ie 32 = 5 or 32/9= 5 remainder
