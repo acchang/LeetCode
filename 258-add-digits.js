b7870650b798b407fa58112b0408d1de9322cab2
Andrew Chang <acchang4@gmail.com>
2023-04-26 09:15:43 -0400

    done

--- 258-add-digits.js
+++ 258-add-digits.js
@@ -1,13 +1,13 @@
 // https://leetcode.com/problems/add-digits/
 
-
 /**
  * @param {number} num
  * @return {number}
  */
 var addDigits = function(num) {
     let numString = num.toString()
-    let answer = 0
+    if (numString.length < 2){return num}
+    let answer 
     while (numString.length > 1){
         answer = 0
         for (let i=0; i<numString.length; i++){
