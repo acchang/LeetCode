54436ca75e9f76f406754d3de409ab1fdc52e522
Andrew Chang <acchang4@gmail.com>
2023-04-27 10:02:39 -0400

    done

---
+++ 319-bulb-switcher.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/bulb-switcher/description/
+// helpful explanation: https://www.youtube.com/watch?v=lyZhLR292yY
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function(n) {
+    return Math.floor(Math.sqrt(n))
+};
+
