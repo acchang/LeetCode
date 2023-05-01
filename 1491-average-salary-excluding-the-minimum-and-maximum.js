70579ea4bc33f74b54164e56cacd0b0484ab455e
Andrew Chang <acchang4@gmail.com>
2023-05-01 07:18:32 -0400

    alt

--- 1491-average-salary-excluding-the-minimum-and-maximum.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum.js
@@ -8,3 +8,20 @@ var average = function(salary) {
     salary.shift()
     return (salary.reduce((a,b) => a+b))/salary.length
 };
+
+// an alt that doesn't use sort but is not faster. Just finds the max and min in O(n), subtracts them and adds the rest
+
+var average = function(salary) {
+    let val=0;
+    let max= -Infinity;
+    let min= Infinity;
+    for(let el of salary){
+        val+=el;
+    max<el?max=el:max=max;
+    min>el?min=el:min=min
+    }
+
+ let ans= val-(max+min);
+ ans= ans/(salary.length-2);
+ return ans
+};
