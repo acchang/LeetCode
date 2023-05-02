1b25279bf704606fb68b8653ed0c63d68088d2c6
Andrew Chang <acchang4@gmail.com>
2023-05-01 23:24:49 -0400

    passes all

--- 1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var arraySign = function(nums) {
-    let product = nums.reduce( (a,c) => a * c)
+    let product
+    nums.includes(0) ? product = 0 : product = nums.reduce( (a,c) => a * c)
     return signFunc(product)
 };
 
 var signFunc = function(x){
     if (x > 0){return 1}
-    else if (x<0){return -1}
-    else if (x===0){return 0}
+    else if (x < 0){return -1}
+    else if (x == 0){return 0}
 };
