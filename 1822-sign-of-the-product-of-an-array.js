c6c9988ff25549ca6916d99c5df42f68ef7d7e8e
Andrew Chang <acchang4@gmail.com>
2023-05-01 23:17:25 -0400

    done

---
+++ 1822-sign-of-the-product-of-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let product = nums.reduce( (a,c) => a * c)
+    return signFunc(product)
+};
+
+var signFunc = function(x){
+    if (x > 0){return 1}
+    else if (x<0){return -1}
+    else if (x===0){return 0}
+};
