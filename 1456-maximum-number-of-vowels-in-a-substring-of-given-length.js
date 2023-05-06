4a7012729cb963e3629179dced25f04d50aeb6e9
Andrew Chang <acchang4@gmail.com>
2023-05-05 20:48:20 -0400

    done

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -5,14 +5,14 @@
  */
 var maxVowels = function(s, k) {
     let counter = 0
-    let max = 0
     for (let i=0; i<k; i++){
          if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o'|| s[i]=='u'){counter++}
     }
+    let max = counter
     for (let j=k;j<s.length;j++){
         if (s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o'|| s[j]=='u'){counter++}
         if (s[j-k]=='a' || s[j-k]=='e' || s[j-k]=='i' || s[j-k]=='o'|| s[j-k]=='u'){counter--}
     if (counter > max){max = counter}
     }
     return max
-};gi
+};
