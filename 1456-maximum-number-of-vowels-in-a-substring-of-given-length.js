0e4620d783072bf49714ab6bc427052017898a1f
Andrew Chang <acchang4@gmail.com>
2023-05-05 20:32:30 -0400

    first tests passed

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -6,15 +6,13 @@
 var maxVowels = function(s, k) {
     let counter = 0
     let max = 0
-    for (let i=0;i<s.length-k+1;i++){
-        let chunk = s.slice(i,i+k)
-        for (let j=0; j<chunk.length;j++){
-            if (chunk[j]=='a' || chunk[j]=='e' || chunk[j]=='i' || chunk[j]=='o'|| chunk[j]=='u'){counter++}
-        }
-    if (counter>max){max = counter} 
-    counter = 0
+    for (let i=0; i<k; i++){
+         if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o'|| s[i]=='u'){counter++}
+    }
+    for (let j=k;j<s.length;j++){
+        if (s[j]=='a' || s[j]=='e' || s[j]=='i' || s[j]=='o'|| s[j]=='u'){counter++}
+        if (s[j-k]=='a' || s[j-k]=='e' || s[j-k]=='i' || s[j-k]=='o'|| s[j-k]=='u'){counter--}
+    if (counter > max){max = counter}
     }
     return max
-};
-
-// keep track of changes in the window to save on memory
+};gi
