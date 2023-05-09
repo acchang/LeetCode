6820fac253416e5b1f9aaa343435fa0c7f7fc401
Andrew Chang <acchang4@gmail.com>
2023-05-09 12:00:22 -0400

    done, using pointers

--- 54-spiral-matrix.js
+++ 54-spiral-matrix.js
@@ -11,20 +11,16 @@ var spiralOrder = function(matrix) {
 // top row
          for (let i=left; i<right; i++){output.push(matrix[top][i])}
          top++
-         console.log("top done")
 // right column
         for (let j=top; j<bottom;j++){output.push(matrix[j][right-1])}
         right--
         if (left>=right || top>=bottom){break} 
-        console.log("right done")
 // bottom row
         for (let k=right-1; k>left-1; k--){output.push(matrix[bottom-1][k])}
         bottom--
-        console.log("bottom done")
 // left column
-        for (let l=bottom-1; l<top-1; l--){output.push(matrix[left][l])}
+        for (let l=bottom-1; l>top-1; l--){console.log(l, left), output.push(matrix[l][left])}
         left++
-        console.log("left done")
     }
     return output
 };
