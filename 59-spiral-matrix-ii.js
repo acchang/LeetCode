b711ca0d841e40399f238a1abef99aae5310dd94
Andrew Chang <acchang4@gmail.com>
2023-05-10 19:03:49 -0400

    add notes

--- 59-spiral-matrix-ii.js
+++ 59-spiral-matrix-ii.js
@@ -31,3 +31,29 @@ var generateMatrix = function(n) {
     }
     return output
 };  
+
+// From the editorial, changing the while loop to a different kind of for loop:
+// uses insight that layers is n+1/2, slightly faster
+
+var generateMatrix = function(n) {
+    let output = Array(n).fill().map(()=>Array(n).fill())
+    let counter = 1
+    let left = 0, right = n, top = 0, bottom = n;
+    for (let i=0; i< (n+1)/2; i++){
+// top row
+        for (let i=left; i<right; i++){(output[top]).splice(i,1,counter); counter++}
+        top++
+// right column
+        for (let j=top; j<bottom;j++){output[j].splice(right-1,1,counter); counter++}
+        right--
+// bottom row
+        for (let k=right-1; k>left-1; k--){(output[bottom-1]).splice(k,1,counter); counter++}
+        bottom--
+// left column
+        for (let l=bottom-1; l>top-1; l--){output[l].splice(left,1,counter); counter++}
+        left++
+    }
+    return output
+};  
+
+
