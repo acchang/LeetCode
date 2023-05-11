cd5648f67d0d9b532d44e1285f355fcfbaaa31cf
Andrew Chang <acchang4@gmail.com>
2023-05-11 09:29:41 -0400

    DP problem

--- 1035-uncrossed-lines.js
+++ 1035-uncrossed-lines.js
@@ -8,18 +8,14 @@
  * @return {number}
  */
 var maxUncrossedLines = function(nums1, nums2) {
-// };
-// var longestCommonSubsequence = function(a, b) {
     const matrix = Array(nums1.length + 1).fill().map(() => Array(nums2.length + 1).fill(0));
-    // create a matrix of a.length + 1
-    // for all those spaces, it will be b.length + 1
 
 for(let i = 1; i < nums1.length + 1; i++) {
     // skip first row, start on down
     for(let j = 1; j < nums2.length + 1; j++) {
     // skip first slow, go across
         if(nums1[i-1] === nums2[j-1]) {
-    // if first a is the same as first b
+    // if first nums1 is the same as first nums2
             matrix[i][j] = 1 + matrix[i-1][j-1];
     // then that plot get a 1 added to the total
         } else {
@@ -29,5 +25,5 @@ for(let i = 1; i < nums1.length + 1; i++) {
     }
 }
 // return the last plot
-return matrix[nums1.length][nums2.length].length;
+return matrix[nums1.length][nums2.length];
 }
