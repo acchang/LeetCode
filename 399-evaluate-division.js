4715a2ccbd3887e8c217a91d6df07519a1a0556f
Andrew Chang <acchang4@gmail.com>
2023-05-20 11:33:35 -0400

    incomplete

--- 399-evaluate-division.js
+++ 399-evaluate-division.js
@@ -1,3 +1,93 @@
+//https://leetcode.com/problems/evaluate-division/description/
+
+/**
+ * @param {string[][]} equations
+ * @param {number[]} values
+ * @param {string[][]} queries
+ * @return {number[]}
+ */
+
+// I understand about 75% of this one, get thrown off by DFS
+
+var calcEquation = function(equations, values, queries) {
+    const adjList = new Map();
+    
+    // Initialize the graph
+    for (let i = 0; i < equations.length; i++) {
+        adjList.set(equations[i][0], []);
+        adjList.set(equations[i][1], []);
+    }
+
+    // Build the graph
+    for (let i = 0; i < equations.length; i++) {
+        const u = equations[i][0];
+        const v = equations[i][1];
+        const weight = values[i];
+        
+        // u to v
+        adjList.get(u).push([v,weight]);
+        
+        // v to u
+        adjList.get(v).push([u, 1/weight]);
+    }
+// this produces a map of each of its nodes and its relationship
+
+    const res = [];
+// Then we initialize a results array to process each query
+
+    for (let i = 0; i < queries.length; i++) {
+        // divisor
+        const src = queries[i][0];
+        
+        // dividend
+        const dest = queries[i][1];
+        const seen = new Set();
+        const val = dfs(adjList, src, src, dest, 1, seen);
+// use a dfs to process the queries        
+        
+        if (val === false) {
+            res.push(-1);
+        } else {
+            res.push(val);
+        }
+    }
+    console.log(res)
+    return res;
+}
+
+
+// In the case that there are other multiple valid paths from source to destination, ultimately, the quotient is the same
+// returning the first then works.
+var dfs = function(adjList, src, curr, dest, quotient, seen) {
+    console.log("AL", adjList, "src", src, "curr", curr, "dest", dest, "quo",quotient, "seen", seen)
+    //destination or source is non existent
+    if (!adjList.has(dest) || !adjList.has(src)) {
+        return -1;
+    }
+    //source is same as destination i.e. self-loop.
+    if (src === dest) {
+        return 1;
+    }
+    // Reached destination
+    if (curr === dest) {
+        return quotient;
+    }
+    seen.add(curr);
+    const neighbors = adjList.get(curr);
+
+    for (let i = 0; i < neighbors.length; i++) {
+        if (seen.has(neighbors[i][0])) {
+            continue;
+        }
+        const val = dfs(adjList, src, neighbors[i][0], dest, quotient * neighbors[i][1], seen);
+        // if any value was found we return because the only other cases would be having a cycle, or reaching our destination or path.
+        if (val !== false) return val;
+    }
+    // We only return false if we've searched all neighbors and there's no path to destination; however we know that src and dest are in adjList, thus it may just be disconnected, but this may return from different levels of recursion.
+    return false;
+}
+
+
 
 
 
