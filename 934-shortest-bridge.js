14d1e229bdfbfbab9d6b5069ec382b8658b3b243
Andrew Chang <acchang4@gmail.com>
2023-05-21 07:53:28 -0400

    try

---
+++ 934-shortest-bridge.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestBridge = function(grid) {
+    
+};
+
+11111
+10001
+10101
+10001
+11111
+
+010
+000
+001
+
+01
+10
+
+find the 1 that has 0 on all sides
+expand out until you hit a 1
