9398d9bef0672ea29a00c5ac39c87f9ab6819c31
Andrew Chang <acchang4@gmail.com>
2023-06-21 19:54:29 -0400

    alt solution

--- 2448-minimum-cost-to-make-array-equal.js
+++ 2448-minimum-cost-to-make-array-equal.js
@@ -38,3 +38,67 @@ now equalize to 2
 3 to go to 2 needs 1 unit * 3 cost = -3 + 12 sum = 9
 4th to got 2 need 1 unit * 1 cost = -1 + 9 sum = 8
 */
+
+// alt solution, no BigInt needed:
+
+function minCost (nums, costs) {
+    const n = nums.length
+    let bank = 0
+
+    for (let i = n - 1; i > -1; --i) {
+        nums[i] = [nums[i], costs[i]]
+        bank += costs[i]
+    }
+
+    nums.sort((b, a) => a[0] - b[0])    
+// create 2D array, sort it
+// bank is what it cost to move everthing by 1
+
+    let minCost = 0
+
+    const base = nums[nums.length - 1][0]
+
+// base is the biggest num
+
+    for (let i = n - 1; i > -1; --i) {
+        // console.log(i, nums[i][0], "minus", base, "times", nums[i][1])
+        minCost += (nums[i][0] - base) * nums[i][1]
+        // console.log(minCost)
+    }
+// move backward through array nums, 
+// mincost is the current minus the smallest num times the cost of each
+
+    let prevCost = minCost
+
+// mincCost is now prevCost
+
+    let left = bank
+// bank is as if all nums needs to go up by 1
+    let right = 0
+// and right is zero
+
+console.log(nums)
+    for (let i = n - 2; i > -1; --i) {
+// from the second lowest going up
+        const prev = nums[i + 1]
+console.log("final loop", i, prev)
+        right += prev[1]
+        left -= prev[1]
+console.log("right:", right, "left:", left)
+// I don't need to sum again bc I got prefix and suffix at nums -1
+// whenever the nums change, then the delta changes
+/* at zero, the first layer is 20
+/* at 1, the first layer adds on 20 again, but 
+
+5 3 2  1
+1 3 14 2
+
+*/
+        const delta = nums[i][0] - prev[0]
+        prevCost += delta * (right - left)
+    
+        if (prevCost < minCost) minCost = prevCost
+    }
+
+    return minCost
+}
