0fde8d5aa6c4be3b55ee2a1be83efea254d332bd
Andrew Chang <acchang4@gmail.com>
2023-06-21 12:47:28 -0400

    almost there

--- 2448-minimum-cost-to-make-array-equal.js
+++ 2448-minimum-cost-to-make-array-equal.js
@@ -11,11 +11,14 @@ var minCost = function(nums, cost) {
     let sortedNums = nums.sort((a,b) => a - b)
     let answer = sortedNums.map((x,i) => (x-sortedNums[0])*numsCostObj[sortedNums[i]]).reduce((a,c) => a + c, 0)
     for (let i=1; i<sortedNums.length;i++){
-
-        sortedNums[i]-sortedNums[i-1]
-
+            let multiplier = sortedNums[i]-sortedNums[i-1]
+            let left =             
+            left * multiplier positive
+            right * muliplier negative
+            let temp = answer + left + right
+            if (temp < answer){answer = temp}
     }
-
+    return answer
 };
 
 /* step 2
