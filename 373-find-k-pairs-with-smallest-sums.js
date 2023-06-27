4ef080733af71609668dd7af75f2b4416d992804
Andrew Chang <acchang4@gmail.com>
2023-06-27 13:10:11 -0400

    heap problem

---
+++ 373-find-k-pairs-with-smallest-sums.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+    let m = nums1
+    let n = nums2
+    let ans = []
+    let visited = []
+};
