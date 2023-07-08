f6911465ffaf97dd59fe3035a555c95d8d3275a2
Andrew Chang <acchang4@gmail.com>
2023-07-07 20:49:07 -0400

    attempt

---
+++ 2024-maximize-the-confusion-of-an-exam.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+var maxConsecutiveAnswers = function(answerKey, k) {
+    let left = 0, charCount = 0, maxT = 0
+    for (let right=0; right<answerKey.length; right++){
+      if (answerKey[right] === "T"){charCount++}
+      while (charCount > k){
+        if(answerKey[left+1] == "T"){charCount--}
+      }
+      if (right-left+1 > maxT){maxT=right-left+1}
+    }
+    
+    left = 0, charCount = 0, maxF = 0
+    for (let right=0; right<answerKey.length; right++){
+      if (answerKey[right] === "F"){charCount++}
+      while (charCount > k){
+        if(answerKey[left+1] == "F"){charCount--}
+      }
+      if (right-left+1 > maxF){maxF=right-left+1}
+    }
+
+  return Math.max(maxT, maxF)
+
+};
