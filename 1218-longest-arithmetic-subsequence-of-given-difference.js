5674991f832fbb037fbd999b1d0ff1b2d79b7d09
Andrew Chang <acchang4@gmail.com>
2023-07-14 20:50:14 -0400

    needs some work

--- 1218-longest-arithmetic-subsequence-of-given-difference.js
+++ 1218-longest-arithmetic-subsequence-of-given-difference.js
@@ -6,6 +6,17 @@
  * @return {number}
  */
 var longestSubsequence = function(arr, difference) {
+    let dp = {}
+    let before_a = 0
+    let answer
+    for (let i=0; i<arr.length; i++){
+        if (dp[arr[i]]){before_a = dp[arr[i]-difference]} else before_a = 0;
+        dp[arr[i]] = before_a + 1;
+        answer = Math.max(answer, dp[arr[i]])
+        }
+        return answer
+    };
+    
     // one pass
     // hash table?
     // if diff positive, start from min, add, see if it exist
