bb11c3a5353f4408d2c753fa2f3fd020f6912ab0
Andrew Chang <acchang4@gmail.com>
2023-07-19 17:17:58 -0400

    73% run, 69% memory

--- 435-non-overlapping-intervals.js
+++ 435-non-overlapping-intervals.js
@@ -1,19 +1,17 @@
-/**
- * @param {number[][]} intervals
- * @return {number}
- */
+// https://leetcode.com/problems/non-overlapping-intervals/submissions/
+
 /**
  * @param {number[][]} intervals
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0])
     let res = 0
     let endValue = intervals[0][1];
-    intervals.sort((a, b) => a[0] - b[0])
-    console.log(intervals)
     for (let i=1; i<intervals.length; i++){
-        if (intervals[i][0]<endValue && ){res++} else {endValue=intervals[i][1]}
-        console.log(endValue, res)
+        if (intervals[i][0]<endValue && intervals[i][1]>endValue){res++}
+        else if (intervals[i][0]<endValue && intervals[i][1]<=endValue){;res++; endValue=intervals[i][1]}
+        else {endValue=intervals[i][1]}
     }
     return res
 };
