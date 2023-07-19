e455183f55fd338d11ac30eab3c8d1d5f6e1d03a
Andrew Chang <acchang4@gmail.com>
2023-07-18 22:08:05 -0400

    finer points

--- 435-non-overlapping-intervals.js
+++ 435-non-overlapping-intervals.js
@@ -2,11 +2,18 @@
  * @param {number[][]} intervals
  * @return {number}
  */
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
 var eraseOverlapIntervals = function(intervals) {
     let res = 0
+    let endValue = intervals[0][1];
     intervals.sort((a, b) => a[0] - b[0])
+    console.log(intervals)
     for (let i=1; i<intervals.length; i++){
-        if (intervals[i][0]>=intervals[i-1][1]){continue}
-        if (intervals[i][0]<intervals[i-1][1]){res++}
+        if (intervals[i][0]<endValue && ){res++} else {endValue=intervals[i][1]}
+        console.log(endValue, res)
     }
+    return res
 };
