e5341d82b9b08ca41b5ed71289fe6b41b2a674c0
Andrew Chang <acchang4@gmail.com>
2023-07-20 13:35:30 -0400

    fails in 200ish of 250

--- 735-asteroid-collision.js
+++ 735-asteroid-collision.js
@@ -7,11 +7,15 @@
 var asteroidCollision = function(asteroids) {
     let astStack = [asteroids[0]]
     for (let i=1; i<asteroids.length; i++){
+        console.log(i)
         if (asteroids[i]<0 && astStack[astStack.length-1]>0){
-                        while (Math.abs(asteroids[i]) > astStack[astStack.length-1]){astStack.pop()}
-            if (Math.abs(asteroids[i]) == astStack[astStack.length-1]){astStack.pop()}
+            while (Math.abs(asteroids[i]) > Math.abs(astStack[astStack.length-1])){astStack.pop()}
+        if (Math.abs(asteroids[i]) == astStack[astStack.length-1]){astStack.pop(); continue} 
+        if (asteroids[i] == astStack[astStack.length-1]){astStack.push(asteroids[i]); continue} 
+
         }
         else {astStack.push(asteroids[i])}
+        console.log(astStack)
     }
     return astStack
 };
