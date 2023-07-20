cfac3efcfd086e395a9e74742fc4c9f396443a72
Andrew Chang <acchang4@gmail.com>
2023-07-20 14:54:19 -0400

    44% run 25% mem

--- 735-asteroid-collision.js
+++ 735-asteroid-collision.js
@@ -1,21 +1,25 @@
 // https://leetcode.com/problems/asteroid-collision/description/
 
+
 /**
  * @param {number[]} asteroids
  * @return {number[]}
  */
 var asteroidCollision = function(asteroids) {
+    let flag = true
     let astStack = [asteroids[0]]
     for (let i=1; i<asteroids.length; i++){
-        console.log(i)
-        if (asteroids[i]<0 && astStack[astStack.length-1]>0){
-            while (Math.abs(asteroids[i]) > Math.abs(astStack[astStack.length-1])){astStack.pop()}
-        if (Math.abs(asteroids[i]) == astStack[astStack.length-1]){astStack.pop(); continue} 
-        if (asteroids[i] == astStack[astStack.length-1]){astStack.push(asteroids[i]); continue} 
-
-        }
-        else {astStack.push(asteroids[i])}
-        console.log(astStack)
+        flag=true
+        if (astStack[astStack.length-1]>0 && asteroids[i]<0){
+            while (
+                (Math.abs(asteroids[i]) > (astStack[astStack.length-1]))
+                &&
+                (astStack[astStack.length-1]>0 && asteroids[i]<0)
+                ){astStack.pop()};
+        if (Math.abs(asteroids[i]) == astStack[astStack.length-1]){astStack.pop();flag=false};
+        if (Math.abs(asteroids[i]) < astStack[astStack.length-1]){flag=false}
+        };
+        if (flag == true){astStack.push(asteroids[i])}
     }
     return astStack
 };
