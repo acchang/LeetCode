67258d0209f05f20b34b38b4e957a551cf214b01
Andrew Chang <acchang4@gmail.com>
2023-07-21 19:28:41 -0400

    done

--- 300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence.js
@@ -16,3 +16,40 @@ var lengthOfLIS = function(nums) {
     }
     return Math.max(...listOfLIS)
 };
+
+// alt: 16% run 72% memory, about the same
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    let listOfLIS = new Array(nums.length).fill(1);
+    let longest = 1
+    for (let i=nums.length-1;i>=0;i--){
+        for (let j=i; j<nums.length;j++){
+            if (nums[i] < nums[j]){listOfLIS[i] = Math.max(listOfLIS[i], listOfLIS[j]+1)}
+            longest= Math.max(longest, listOfLIS[i])
+        }
+
+    }
+    return longest
+};
+
+// from solutions: 25% run 45% memory
+
+
+const lengthOfLIS = (nums) => {
+	let dp = new Array(nums.length).fill(1);
+	let longest = 1;
+	for (let i = 1; i < nums.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (nums[j] < nums[i]) {
+				dp[i] = Math.max(dp[i], dp[j]+1);
+				longest = Math.max(longest, dp[i]);
+			}
+		}
+	}
+	return longest;
+}
+
