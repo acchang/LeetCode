1e58545526fc0d8cf319ebfacc796dcc057a904c
Andrew Chang <acchang4@gmail.com>
2023-07-21 19:02:43 -0400

    just one test fails

--- 300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence.js
@@ -4,13 +4,16 @@
  */
 var lengthOfLIS = function(nums) {
     let listOfLIS = new Array(nums.length).fill(0);
-    for (let i=nums.length-1;i<=0;i--){
+    console.log(listOfLIS)
+    for (let i=nums.length-1;i>=0;i--){
         let currentChoices = [1]
         for (let j=i; j<nums.length-1;j++){
-            if (i < nums[j]){currentChoices.push(1+listOfLIS[j])}
+            console.log('CC', j, currentChoices)
+            if (nums[i] < nums[j]){currentChoices.push(1+listOfLIS[j])}
         }
         let LIS = Math.max(...currentChoices)
         listOfLIS[i] = LIS
+        console.log(i, currentChoices)
     }
-    return Math.max(listOfLIS)
+    return Math.max(...listOfLIS)
 };
