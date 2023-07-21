a85cf07b87de2e81b35aef1986072adee8cba83c
Andrew Chang <acchang4@gmail.com>
2023-07-21 19:17:39 -0400

    passed all tests

--- 300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence.js
@@ -1,22 +1,18 @@
 // idea from https://www.youtube.com/watch?v=cjWnW0hdF1Y&t=10s
 
-
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
     let listOfLIS = new Array(nums.length).fill(0);
-    console.log(listOfLIS)
     for (let i=nums.length-1;i>=0;i--){
         let currentChoices = [1]
-        for (let j=i; j<nums.length-1;j++){
-            console.log('CC', j, currentChoices)
+        for (let j=i; j<nums.length;j++){
             if (nums[i] < nums[j]){currentChoices.push(1+listOfLIS[j])}
         }
         let LIS = Math.max(...currentChoices)
         listOfLIS[i] = LIS
-        console.log(i, currentChoices)
     }
     return Math.max(...listOfLIS)
 };
