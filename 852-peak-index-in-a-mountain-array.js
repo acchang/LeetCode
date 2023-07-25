d9e2c24bd38fa6ed1fbd93bb3ca8369ec2816f73
Andrew Chang <acchang4@gmail.com>
2023-07-24 20:25:05 -0400

    dobe

--- 852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array.js
@@ -3,10 +3,7 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-    // sliding window
-    let output = 0
     for (let i= 1; i<arr.length-1; i++){
-        if (arr[i-1]<arr[i] && arr[i] >arr[i+1]){output++}
+        if (arr[i-1]<arr[i] && arr[i] >arr[i+1]){return i}
     }
-    return output
 };
