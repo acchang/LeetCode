4c246903ece228bb44eddfe9b9ab1f9bebec6fd6
Andrew Chang <acchang4@gmail.com>
2023-07-24 20:26:37 -0400

    add binary search

--- 852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array.js
@@ -7,3 +7,20 @@ var peakIndexInMountainArray = function(arr) {
         if (arr[i-1]<arr[i] && arr[i] >arr[i+1]){return i}
     }
 };
+
+// also by binary search, find the highest number
+
+var peakIndexInMountainArray = function (arr) {
+    let start = 0, end = arr.length - 1, mid;
+    while (start < end) {
+        mid = Math.floor((start + end) / 2);
+        if (arr[mid] < arr[mid + 1]) {
+            start = mid + 1
+        }
+        else {
+            end = mid;
+        }
+    }
+    return start;
+};
+
