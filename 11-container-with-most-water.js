c86b618afd2530849d6525b76134f6b8ce01b254
Andrew Chang <acchang4@gmail.com>
2023-10-08 06:58:23 -0400

    O(n)

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -16,3 +16,25 @@ var maxArea = function(height) {
     }
     return maxArea
 }
+
+// better, faster:
+
+function maxArea(height){
+    let low = 0;
+    let high = height.length - 1;
+    let ans = 0;
+  
+    while (low <= high) {
+      const length = Math.min(height[low], height[high]);
+      const breadth = high - low;
+      const area = length * breadth;
+  
+      ans = Math.max(ans, area);
+  
+      if (height[low] < height[high]) low++;
+      else high--;
+    }
+    return ans;
+  };
+
+  
