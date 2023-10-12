9678a03b4c167471a002a66282c0485c8fdd60fe
Andrew Chang <acchang4@gmail.com>
2023-10-12 07:59:40 -0400

    new solution

--- 15-3sum.js
+++ 15-3sum.js
@@ -104,3 +104,30 @@ function threeSum(nums) {
 
 	return results
 };
+
+
+
+// from https://leetcode.com/problems/3sum/solutions/3601410/using-two-pointers-line-by-line-code-explained/
+
+
+var threeSum = function(nums) {
+    let ans = [];
+    nums.sort((a,b) => a-b)
+    for (let i=0; i<nums.length-2; i++){
+        if (i>0 && nums[i] === nums[i-1])continue;
+        let l=i+1, r=nums.length-1;
+    while(l<r){
+        let sum = nums[i] + nums[l] + nums[r];
+        if (sum===0){
+            ans.push([nums[i], nums[l], nums[r]])
+            while (nums[l] === nums[l+1]){l++}
+            while (nums[r] === nums[r+1]){r--}
+            l++
+            r--
+        }
+        else if (sum<0){l++}
+        else {r--}
+    }
+}
+return ans
+};
