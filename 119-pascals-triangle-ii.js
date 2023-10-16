44057dadfa1931a5fb43b47c337ec2efd59c2085
Andrew Chang <acchang4@gmail.com>
2023-10-15 23:31:42 -0400

    72% fast, 55% memory

--- 119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii.js
@@ -1,3 +1,30 @@
+// fastest:
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let counter = 0
+    let baseArray = [1]
+    let newArray = []
+    while (counter<rowIndex){
+        for (let i=0; i<baseArray.length-1;i++){
+            newArray.push(baseArray[i]+baseArray[i+1])
+        }
+        newArray.push(1)
+        newArray.unshift(1)
+        counter++
+        baseArray = newArray
+        newArray = []
+    }
+    return baseArray
+};
+
+
+
+
+
 /**
  * @param {number} rowIndex
  * @return {number[]}
@@ -20,29 +47,3 @@ var getRow = function(rowIndex) {
     return rowMap[rowIndex]
 };
 
-
-/**
- * @param {number} rowIndex
- * @return {number[]}
- */
-var getRow = function(rowIndex) {
-    // create hash map?
-    let rowMap
-    rowMap[1] = [1[]
-    if (rowIndex == 0){ans.push(1)}
-    if (rowIndex == 1){ans.push(1,1)}
-    else {
-        let focus = [1,1]
-        let counter = 1
-        while (counter<rowIndex){
-            for (let i=0;i<focus.length-1;i++){
-                ans.push(focus[i]+focus[i+1])
-            }
-            ans.push(1)
-            ans.unshift(1)
-            focus = ans
-            counter++
-        }
-    }
-    return ans
-};
