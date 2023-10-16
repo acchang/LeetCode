2a945ae16c43b930a3df1c6d9c2bf2c4b5229bd2
Andrew Chang <acchang4@gmail.com>
2023-10-15 23:18:27 -0400

    passes initial tests

--- 119-pascals-triangle-ii.js
+++ 119-pascals-triangle-ii.js
@@ -3,6 +3,29 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
+    const rowMap = {}
+    rowMap[0] = [1]
+    rowMap[1] = [1,1]
+    let rowMapCounter = 1
+    while (rowMapCounter<rowIndex){
+        let newArray = []
+        for (let i=0; i<rowMap[rowMapCounter].length-1;i++){
+            newArray.push(rowMap[rowMapCounter][i]+rowMap[rowMapCounter][i+1])
+        }
+        newArray.push(1)
+        newArray.unshift(1)
+        rowMapCounter++
+        rowMap[rowMapCounter] = newArray 
+    }
+    return rowMap[rowIndex]
+};
+
+
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
     // create hash map?
     let rowMap
     rowMap[1] = [1[]
