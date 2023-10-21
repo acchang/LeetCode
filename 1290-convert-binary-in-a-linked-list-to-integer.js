d3919bd43e41af117ff090fabf4fe0e4a253831b
Andrew Chang <acchang4@gmail.com>
2023-10-21 19:55:30 -0400

    simpler version

--- 1290-convert-binary-in-a-linked-list-to-integer.js
+++ 1290-convert-binary-in-a-linked-list-to-integer.js
@@ -22,3 +22,27 @@ var getDecimalValue = function(head) {
     console.log("end", bin)
     return parseInt(bin, 2)
 };
+
+
+// Or, simpler:
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    let bin = ""
+    while (head){
+        bin += head.val.toString()
+        head = head.next
+    }
+    return parseInt(bin, 2)
+};
+
