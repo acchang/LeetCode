487c24f03fcd1a17657ac5e95ff8464376714e06
Andrew Chang <acchang4@gmail.com>
2023-11-08 11:20:30 -0500

    a plan

---
+++ 2849-determine-if-a-cell-is-reachable.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} fx
+ * @param {number} fy
+ * @param {number} t
+ * @return {boolean}
+ */
+var isReachableAtTime = function(sx, sy, fx, fy, t) {
+    // xy to xy in t seconds
+    gap between sx and fx is horizontal movement
+    gap between sy and fy is vertical movement
+    save time with diagonals
+    example 5 across, 3 down
+    singles when they overlap except for 1st and last (minus 2 on y axis)
+    8 no diagonals, with diags is 6
+
+example two
+    across: 7-3 = 4
+    down: 1-3 = 2
+    6-2 = 4,
+// figured out the lgo
+
+};
