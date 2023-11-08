e748f6c4216f865ef5a89652f2c511dacbf21688
Andrew Chang <acchang4@gmail.com>
2023-11-08 12:20:20 -0500

    dumb same start & end conditions

--- 2849-determine-if-a-cell-is-reachable.js
+++ 2849-determine-if-a-cell-is-reachable.js
@@ -7,21 +7,7 @@
  * @return {boolean}
  */
 var isReachableAtTime = function(sx, sy, fx, fy, t) {
-    let steps = Math.abs(fx-sx)+Math.abs(fy-sy)-2
-    return steps <= t 
+    if (sx == fx && sy == fy && t == 1){return false}
+    else if (sx == fx && sy == fy && t != 1){return true}
+    else {return Math.max(Math.abs(fx-sx), Math.abs(fy-sy))<= t}
 }
-    //     // xy to xy in t seconds
-//     gap between sx and fx is horizontal movement
-//     gap between sy and fy is vertical movement
-//     save time with diagonals
-//     example 5 across, 3 down
-//     singles when they overlap except for 1st and last (minus 2 on y axis)
-//     8 no diagonals, with diags is 6
-
-// example two
-//     across: 7-3 = 4
-//     down: 1-3 = 2
-//     6-2 = 4,
-// // figured out the logic
-
-};
