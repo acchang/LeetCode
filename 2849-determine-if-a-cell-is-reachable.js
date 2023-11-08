c31325234f21e369c9996082b646e0f67625431e
Andrew Chang <acchang4@gmail.com>
2023-11-08 12:27:15 -0500

    done

--- 2849-determine-if-a-cell-is-reachable.js
+++ 2849-determine-if-a-cell-is-reachable.js
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/description/?envType=daily-question&envId=2023-11-08
+
 /**
  * @param {number} sx
  * @param {number} sy
@@ -11,3 +13,6 @@ var isReachableAtTime = function(sx, sy, fx, fy, t) {
     else if (sx == fx && sy == fy && t != 1){return true}
     else {return Math.max(Math.abs(fx-sx), Math.abs(fy-sy))<= t}
 }
+
+// the edge cases are to deal with the fact that you have to reach it in exactly that number of steps. 1 is the edge case when starting and ending in the same spot because you need at least 2 steps.
+
