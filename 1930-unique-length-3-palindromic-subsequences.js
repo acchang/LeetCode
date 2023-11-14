47ff278470f08154faadb7ec6eef6f7c766e501d
Andrew Chang <acchang4@gmail.com>
2023-11-14 10:20:38 -0500

    done

--- 1930-unique-length-3-palindromic-subsequences.js
+++ 1930-unique-length-3-palindromic-subsequences.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var countPalindromicSubsequence = function(s) {
-    
+    // see if there are two ends in the word
+    // count the number of unique letters between the ends
+    // taking the first and last instance will not repeat ends
+    // create set to prevent repeats and use size to count
+    let ans = 0
+    for (let i=0; i<s.length; i++){
+        if (i == s.indexOf(s[i]) && s.indexOf(s[i])<s.lastIndexOf(s[i])){
+            ans += new Set(s.slice(s.indexOf(s[i])+1, s.lastIndexOf(s[i])).split("")).size
+        }else {continue}
+    }
+    return ans
 };
