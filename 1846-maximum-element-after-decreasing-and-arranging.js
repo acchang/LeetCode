cf19200963c1bcb56fea8348ac780c23ae63a3a1
Andrew Chang <acchang4@gmail.com>
2023-11-15 17:40:04 -0500

    cleaned up

--- 1846-maximum-element-after-decreasing-and-arranging.js
+++ 1846-maximum-element-after-decreasing-and-arranging.js
@@ -10,14 +10,3 @@ var maximumElementAfterDecrementingAndRearranging = function(arr) {
     }
     return ans
 }
-
-    // reduce any
-    // rearrange all
-    // if the first element must be 1 and difference is max of 1
-    // then max is length if any element is bigger than length
-    // but they need elements to be within 1 of each other
-    // OR if all elements smaller than length
-    // 2 4 1 2 1 is not 4 bc every element must be no diff than 1, would be 3
-    // so then sort 1 1 2 2 4
-    // if it includes 1 then lead with it
-    // if not
