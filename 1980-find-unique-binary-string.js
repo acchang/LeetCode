02dbbd421c76a60f68f78136bc5cb4493051bfd0
Andrew Chang <acchang4@gmail.com>
2023-11-16 14:54:52 -0500

    cantors diagonal works

--- 1980-find-unique-binary-string.js
+++ 1980-find-unique-binary-string.js
@@ -3,6 +3,20 @@
  * @return {string}
  */
 var findDifferentBinaryString = function(nums) {
+    // cantors goes through the array and takes the opposite each digit of the index of the number?
+        let ans = ""
+        for (let i=0; i<nums.length; i++){
+            if (nums[i][i] == 1){ans +=0} else {ans+=1}
+        }
+        return ans
+    // ans will differ from every string in at least one position. 
+    };
+
+    
+
+
+// this passed but is wrong as the loop only works single 1s.
+var findDifferentBinaryString = function(nums) {
     let n = nums.length
     let template = new Array(n).fill(0)
         let testValue = template.join("");
