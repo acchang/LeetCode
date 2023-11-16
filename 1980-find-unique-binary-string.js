195169e033b81a732b1df724a6f7c02f69113300
Andrew Chang <acchang4@gmail.com>
2023-11-16 14:18:49 -0500

    passed but not right, doesn't check 10101

--- 1980-find-unique-binary-string.js
+++ 1980-find-unique-binary-string.js
@@ -3,57 +3,15 @@
  * @return {string}
  */
 var findDifferentBinaryString = function(nums) {
-
-
-    
-    each digit corresp to n max
-    if at max first missing of binary is 0
-    if 1 less than poss is 1,0 
-
-    if not then 0 and 1 for each lower than max
-
-
+    let n = nums.length
+    let template = new Array(n).fill(0)
+        let testValue = template.join("");
+        console.log("FTV", testValue)
+        if (!nums.includes(testValue)){return testValue}
+    for (let i=0; i<template.length;i++){
+        if (template[i] == 0){template[i]=1}
+        testValue = template.join("");
+        console.log("FTV", testValue)
+        if (!nums.includes(testValue)){return testValue}
+    }
 };
-
-// brute force, generate possibilities and ask included
-a b     c
-0 0
-1 1
-1 0
-0 1
-0
-1
-
-22 add all up, 333, 444 so on 
-
-n spaces, n length
-01
-10
-first d is 0 or 1
-second d is 0 or 1
-
-00
-01
-first d is 2 means both 1 unused (has to be 1)
-second d is 1 means 1 unused (so can be 0 or 1)
-
-assemble range and test
-
-
-
-build a array on n
-fill with zeroes
-test
-
-000
-001
-010
-100
-101
-110
-111
-
-00
-10
-10
-11
