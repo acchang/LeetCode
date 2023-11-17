2cbc5e05acfaa7f7081a7d3909127af836fec010
Andrew Chang <acchang4@gmail.com>
2023-11-17 11:07:54 -0500

    alt solution

--- 1877-minimize-maximum-pair-sum-in-array.js
+++ 1877-minimize-maximum-pair-sum-in-array.js
@@ -10,3 +10,16 @@ var minPairSum = function(nums) {
     }
     return Math.max(...pairs)
 };
+
+// a little less on memory, a little more on runtime
+
+var minPairSum = function(nums) {
+    let ans = 0
+    nums.sort((a,b)=>a-b)
+    for (let i=0; i<nums.length/2; i++){
+        let pair = (nums[i] + nums[nums.length-1-i])
+        if (pair > ans){ans = pair}
+    }
+    return ans
+};
+
