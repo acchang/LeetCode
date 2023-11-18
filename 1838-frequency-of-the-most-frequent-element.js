be72fd6f190f8bc14e2f8a00402ced3573322e50
Andrew Chang <acchang4@gmail.com>
2023-11-18 13:08:07 -0500

    passes initial tests

--- 1838-frequency-of-the-most-frequent-element.js
+++ 1838-frequency-of-the-most-frequent-element.js
@@ -3,19 +3,20 @@
  * @param {number} k
  * @return {number}
  */
+
 var maxFrequency = function(nums, k) {
     nums.sort((a,b)=>a-b)
     let left = 0
-    let ans = 0
-    let curr = 0
-    for (let i=nums.length-1; i>-1; i--){
-      let target = nums[i]
-      curr += target
-      while ( ((nums[i]-left+1)*target-curr) > k){
+    let ans = 1
+    for (let right=nums.length-1; right>-1; right--){
+      let target = nums[right]
+      let total = nums.slice(left, right).reduce((acc, cV)=>acc+cV, 0)
+      while ( (((right-left)*target-total) > k) && right !=left){
         left++
+        total = nums.slice(left, right).reduce((acc, cV)=>acc+cV, 0)
       }
-      if ((nums[i]-left+1) > ans){ans = (nums[i]-left+1)} 
-      curr = 0
+      if ((right-left+1) > ans){ans = right-left+1} 
   }
   return ans
 };
+
