84c7ea5b0c8b5c40b44fbfb4711d40213a912f5d
Andrew Chang <acchang4@gmail.com>
2023-12-11 21:17:04 -0500

    better solution

--- 1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array.js
@@ -6,3 +6,23 @@ var maxProduct = function(nums) {
     nums.sort((a,b)=>b-a)
     return (nums[0]-1)*(nums[1]-1)
 };
+
+// also:
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let biggest = 0
+    let secondBiggest = 0
+    for (let i=0; i<nums.length; i++){
+        if (nums[i]>biggest){
+            secondBiggest = biggest;
+            biggest = nums[i]
+        } else {
+            secondBiggest = Math.max(secondBiggest, nums[i])
+        }
+    }
+    return (biggest-1)*(secondBiggest-1)
+};
