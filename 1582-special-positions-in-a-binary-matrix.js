c9252e3aa9e21a319b1fec79db0b2eb11f7e42b7
Andrew Chang <acchang4@gmail.com>
2023-12-12 20:46:19 -0500

    stopped at 87/95

--- 1582-special-positions-in-a-binary-matrix.js
+++ 1582-special-positions-in-a-binary-matrix.js
@@ -4,12 +4,14 @@
  */
 var numSpecial = function(mat) {
     let row = []
+    let column = []
+    let columnParts = []
+    let ans = 0
+    
     for (let i=0;i<mat.length;i++){
         row.push(mat[i].reduce((acc,cV) => acc + cV,0))
         }
-    console.log("row", row)
-    let column = []
-    let columnParts = []
+
     for (let j=0;j<mat.length;j++){
         for (let k=0;k<mat.length;k++){
             columnParts.push(mat[k][j])
@@ -17,8 +19,7 @@ var numSpecial = function(mat) {
         column.push(columnParts.reduce((acc,cV) => acc + cV,0))
         columnParts=[]
     }
-    console.log("column", column)
-    let ans = 0
+    
     for (let l=0;l<mat.length;l++){
         for (let m=0;m<mat.length;m++){
             console.log(mat[l][m])
@@ -29,7 +30,3 @@ var numSpecial = function(mat) {
     }
     return ans
 };
-
-// 100
-// 001
-// 100
