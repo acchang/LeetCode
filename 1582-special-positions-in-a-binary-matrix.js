93ec595744ff423c18ff15fcce73df0489ad1bf6
Andrew Chang <acchang4@gmail.com>
2023-12-12 20:44:18 -0500

    solved?

--- 1582-special-positions-in-a-binary-matrix.js
+++ 1582-special-positions-in-a-binary-matrix.js
@@ -3,21 +3,33 @@
  * @return {number}
  */
 var numSpecial = function(mat) {
-    let horiz = 0
+    let row = []
     for (let i=0;i<mat.length;i++){
-        if (mat.reduce((acc,cV) => acc + cV,0) == 1){horiz++}
+        row.push(mat[i].reduce((acc,cV) => acc + cV,0))
         }
-    console.log(horiz)
+    console.log("row", row)
+    let column = []
     let columnParts = []
-    let vertical = 0
     for (let j=0;j<mat.length;j++){
         for (let k=0;k<mat.length;k++){
             columnParts.push(mat[k][j])
         }
-        if (columnParts.reduce((acc,cV) => acc + cV,0) == 1){vertical++}
-        columnParts=[]   
+        column.push(columnParts.reduce((acc,cV) => acc + cV,0))
+        columnParts=[]
     }
-    console.log(vertical)
-    return Math.max(horiz,vertical)
+    console.log("column", column)
+    let ans = 0
+    for (let l=0;l<mat.length;l++){
+        for (let m=0;m<mat.length;m++){
+            console.log(mat[l][m])
+            if (mat[l][m]==1 && row[l]==1 && column[m]==1){
+                ans++
+            }
+        }
+    }
+    return ans
 };
 
+// 100
+// 001
+// 100
