3c6fa32cc3f912f538c6a0bb6a4e32181d25d74a
Andrew Chang <acchang4@gmail.com>
2023-12-13 21:10:05 -0500

    a little tighter

--- 2482-difference-between-ones-and-zeroes-in-row-and-column.js
+++ 2482-difference-between-ones-and-zeroes-in-row-and-column.js
@@ -3,32 +3,29 @@
  * @return {number[][]}
  */
 var onesMinusZeros = function(grid) {
-    let onesRow = []
-    let onesCol = []
-    let zerosRow = []
-    let zerosCol = []
+    let totalRow = []
+    let totalCol = []
     let diff = []
     let diffRow
-    let oneColSum 
-    let zeroColSum 
+    let totalColSum 
     for (let i=0;i<grid.length;i++){
-        onesRow.push(grid[i].filter((x) => x == 1).length)
-        zerosRow.push(grid[i].filter((x) => x == 0).length)
+        totalRow.push(
+            (grid[i].filter((x) => x == 1).length) - 
+            (grid[i].filter((x) => x == 0).length)
+        )
     }
     for (let j=0;j<grid[0].length;j++){
-            oneColSum = 0
-            zeroColSum = 0
+            totalColSum = 0
         for (let k=0;k<grid.length;k++){
-            if (grid[k][j] == 1){oneColSum++} else {zeroColSum++}
+            if (grid[k][j] == 1){totalColSum++} else {totalColSum--}
         }
-        onesCol.push(oneColSum)
-        zerosCol.push(zeroColSum)
+        totalCol.push(totalColSum)
     }
 
     for (let l=0;l<grid.length;l++){
         diffRow = []
         for (let m=0;m<grid[0].length;m++){
-            diffRow.push(onesRow[l] + onesCol[m] - zerosRow[l] - zerosCol[m])
+            diffRow.push(totalRow[l] + totalCol[m])
         }
         diff.push(diffRow)
     }
