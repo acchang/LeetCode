6273da630eb9733132d7dd86e735c40895723646
Andrew Chang <acchang4@gmail.com>
2023-12-14 21:36:12 -0500

    alt solution

--- 1436-destination-city.js
+++ 1436-destination-city.js
@@ -16,3 +16,22 @@ var destCity = function(paths) {
             // the second that's not in first
     }
 };
+
+// also, faster:
+
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    let hashMap = new Map()
+    for(let i = 0; i < paths.length; i++){
+        hashMap.set(paths[i][0], paths[i][1])
+    }
+
+    for(const city of hashMap.values()){
+        if(!hashMap.has(city)){
+            return city
+        }
+    }
+};
