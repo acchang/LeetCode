6289b978905271b678e514599c8a0cc0ccdeb6b9
Andrew Chang <acchang4@gmail.com>
2023-12-15 20:01:53 -0500

    alt but no improvement

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -6,3 +6,16 @@
 var isAnagram = function(s, t) {
     return s.split("").sort().join("") == t.split("").sort().join("")
 };
+
+// this is an official solution, which is the same speed
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    const ss = s.split('').sort();
+    const tt = t.split('').sort();
+    return ss.join('') === tt.join('');
+};
