3295d9261cef37faa80cd612a0a79e7d8031e7e6
Andrew Chang <acchang4@gmail.com>
2023-12-17 19:46:28 -0500

    alt

--- 1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs.js
@@ -6,3 +6,17 @@ var maxProductDifference = function(nums) {
     nums.sort((a,b)=>b-a)
     return (nums[0]*nums[1]) - (nums[nums.length-1]*nums[nums.length-2])
 };
+
+// alt:
+
+Iterate over each num in nums:
+If num > biggest:
+Update secondBiggest = biggest.
+Update biggest = num.
+Else:
+Update secondBiggest with num if it is larger.
+If num < smallest:
+Update secondSmallest = smallest.
+Update smallest = num.
+Else:
+Update secondSmallest with num if it is smaller.
