c4e65b905d511eeb9f133f1ca527bace655b2730
Andrew Chang <acchang4@gmail.com>
2023-12-19 05:55:09 -0800

    alt works

--- 1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs.js
@@ -9,14 +9,31 @@ var maxProductDifference = function(nums) {
 
 // alt:
 
-Iterate over each num in nums:
-If num > biggest:
-Update secondBiggest = biggest.
-Update biggest = num.
-Else:
-Update secondBiggest with num if it is larger.
-If num < smallest:
-Update secondSmallest = smallest.
-Update smallest = num.
-Else:
-Update secondSmallest with num if it is smaller.
+var maxProductDifference = function(nums) {
+    let biggest = 0
+    let secondBig = 0
+    let smallest = Infinity
+    let secondSmall = Infinity
+    for (num of nums){
+        if (num>biggest){secondBig = biggest; biggest = num}
+        // have to do it this way or else secondBig also is num
+        else if (num>secondBig){secondBig = num}
+        // needs else or it would update secondBig too
+        if (num<smallest){secondSmall=smallest; smallest = num}
+        else if (num<secondSmall){secondSmall = num}
+   }
+   return (biggest*secondBig) - (smallest*secondSmall)
+};
+
+
+// Iterate over each num in nums:
+// If num > biggest:
+// Update secondBiggest = biggest.
+// Update biggest = num.
+// Else:
+// Update secondBiggest with num if it is larger.
+// If num < smallest:
+// Update secondSmallest = smallest.
+// Update smallest = num.
+// Else:
+// Update secondSmallest with num if it is smaller.
