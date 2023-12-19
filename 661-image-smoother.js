28c5628714c81797c7b67781e3b52956c101a9d7
Andrew Chang <acchang4@gmail.com>
2023-12-19 07:59:58 -0800

    just dealing with valid/invalid spaces

--- 661-image-smoother.js
+++ 661-image-smoother.js
@@ -3,12 +3,12 @@
  * @return {number[][]}
  */
 var imageSmoother = function(img) {
-    let smoothImgInner = new Array(img[0]).fill(0)
+    let smoothImgInner = new Array(img[0].length).fill(0)
     let smoothImg = new Array(img.length).fill(smoothImgInner)
 
-    for (let m=0; m<m.length;m++){
-        for (let n=0; n<n.length;n++){
-            let count = 1
+    for (let m=0; m<img.length;m++){
+        for (let n=0; n<img[0].length;n++){
+            let count = 0
             let total = 0
             if (img[m-1][n-1]){total += img[m-1][n-1]; count++}
             if (img[m-1][n]){total += img[m-1][n]; count++}
@@ -18,7 +18,8 @@ var imageSmoother = function(img) {
             if (img[m+1][n-1]){total += [m+1][n-1]; count++}
             if (img[m+1][n]){total += [m+1][n]; count++}
             if (img[m+1][n+1]){total += [m+1][n+1]; count++}
-            smoothImg[m][n]=total/count
+            smoothImg[m][n]=Math.floor(total/count)
+            // console.log(smoothImg)
         }
     }
     return smoothImg
