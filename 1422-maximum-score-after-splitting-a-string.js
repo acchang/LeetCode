302285c03d3e333b1b579361d5fea538c42b3b4d
Andrew Chang <acchang4@gmail.com>
2023-12-21 19:30:08 -0800

    one pass, optimized

--- 1422-maximum-score-after-splitting-a-string.js
+++ 1422-maximum-score-after-splitting-a-string.js
@@ -3,7 +3,8 @@
  * @return {number}
  */
 var maxScore = function(s) {
-    let ones = s.match(/1/g).length;
+    let ones 
+    s.match(/1/g) == null ? ones = 0 : ones = s.match(/1/g).length
     let zeroes = 0
     let max = 0
     for (let i=0; i<s.length-1;i++){
@@ -14,3 +15,23 @@ var maxScore = function(s) {
     }
     return max
 };
+
+/// this is one pass, pretty fancy, a bit faster
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+    let ones = 0
+    let zeroes = 0
+    let max = -Infinity
+    for (let i=0; i<s.length-1;i++){
+        if (s[i] == 1){ones++} else zeroes++
+        console.log(s[i], zeroes, ones, zeroes-ones)
+        if ( (zeroes-ones) > max){max= (zeroes-ones)}
+    }
+    if (s[s.length-1] == 1){ones++}
+    return max + ones
+};
+
