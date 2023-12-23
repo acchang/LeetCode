5d5fcc45420a9cf88a4ada3ab32a8314a0eada30
Andrew Chang <acchang4@gmail.com>
2023-12-22 17:33:08 -0800

    better than most, cut console

--- 1496-path-crossing.js
+++ 1496-path-crossing.js
@@ -6,7 +6,6 @@ var isPathCrossing = function(path) {
     let curLoc = [0,0]
     let locList = ["0-0"]
     for (let i=0;i<path.length;i++){
-        console.log(path[i])
         if (path[i]==="N"){curLoc[1]=curLoc[1]+1;}
         if (path[i]==="S"){curLoc[1]=curLoc[1]-1;}
         if (path[i]==="E"){curLoc[0]=curLoc[0]+1;}
