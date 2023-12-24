8a4e8cf6c2669a7ab4b2b19401d59bff74823a87
Andrew Chang <acchang4@gmail.com>
2023-12-23 20:26:03 -0800

    passed

---
+++ 1758-minimum-changes-to-make-alternating-binary-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    let zeroCount = 0
+    let onesCount = 0
+    //start 0
+    let testEle
+    for (let i=0; i<s.length; i++){
+        if (i%2 == 0){testEle = 0} else {testEle = 1}
+        if (s[i] != testEle){zeroCount++}
+    }
+
+    // start 1
+    for (let i=0; i<s.length; i++){
+        if (i%2 == 0){testEle = 1} else {testEle = 0}
+        if (s[i] != testEle){onesCount++}
+    }
+    
+    return Math.min(zeroCount, onesCount)
+};
