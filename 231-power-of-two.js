af3dcc8f1c4bf0df4c3a5a3de93e075e9c385b29
Andrew Chang <acchang4@gmail.com>
2023-12-27 20:35:38 -0500

    done

--- 231-power-of-two.js
+++ 231-power-of-two.js
@@ -3,17 +3,26 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n, total = 1) {
-    console.log(n-total)
-    console.log("total:", total, "n-t", n-total, total === n, total > n)
+    // console.log(n-total)
+    // console.log("total:", total, "n-t", n-total, total === n, total > n)
     // if ((n-total) == 0){return true} else if ((n-total) < 0){return false}
     // if (n-total <= 0){return ((n-total) == 0)}
     // if (total === n ){return true}
     // else if (total > n){return false}
     if (total === n ){return true} else if (total > n){return false}
     total += total
-    isPowerOfTwo(n, total)
+    return isPowerOfTwo(n, total)
 };
 
+// Will keep getting undefined unless you return the recursive statement.
 
-
-// I keep getting undefined. why?
+// this is also good and more straightforward:
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n == 1){return true} else if (n%2 !=0 || n==0){return false}
+     return isPowerOfTwo(n/2)
+ };
+ 
