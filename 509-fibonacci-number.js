e2344350d2756b18234e76d459f26d901aabb2f2
Andrew Chang <acchang4@gmail.com>
2023-12-27 21:31:39 -0500

    naive approach

--- 509-fibonacci-number.js
+++ 509-fibonacci-number.js
@@ -3,5 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
-    if (n<=2){return 1} else {return fib(n-1)+fib(n-2)}
+    if (n === 0){return 0}
+    else if (n<=2){return 1} 
+    else {return fib(n-1)+fib(n-2)}
 };
