434e5b5b9c8cb9cc74973064a2faf915b50f9437
Andrew Chang <acchang4@gmail.com>
2023-12-28 09:03:52 -0500

    done with dp

--- 509-fibonacci-number.js
+++ 509-fibonacci-number.js
@@ -7,3 +7,22 @@ var fib = function(n) {
     else if (n<=2){return 1} 
     else {return fib(n-1)+fib(n-2)}
 };
+
+
+// using DP:
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    let memo = {}
+    let result
+    for(let i = 0; i<n+1; i++){
+        if (i<2){result=i}
+        else {result = memo[i-1] + memo [i-2]}
+        memo[i]=result
+        console.log(memo)
+    }
+    return memo[n]
+};
