f04f72daf992895f5d27dda303a8df137b131f1a
Andrew Chang <acchang4@gmail.com>
2023-12-27 20:07:37 -0500

    undefined?

--- 231-power-of-two.js
+++ 231-power-of-two.js
@@ -4,12 +4,16 @@
  */
 var isPowerOfTwo = function(n, total = 1) {
     console.log(n-total)
-    console.log("total:", total, "n-t", n-total, (n-total) == 0)
+    console.log("total:", total, "n-t", n-total, total === n, total > n)
     // if ((n-total) == 0){return true} else if ((n-total) < 0){return false}
-    if (n-total <= 0){return ((n-total) == 0)}
+    // if (n-total <= 0){return ((n-total) == 0)}
+    // if (total === n ){return true}
+    // else if (total > n){return false}
+    if (total === n ){return true} else if (total > n){return false}
     total += total
-    console.log(total)
     isPowerOfTwo(n, total)
 };
+
+
 
 // I keep getting undefined. why?
