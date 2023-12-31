415d00f30768a5172402f9bdd9c2f47df8b6ec61
Andrew Chang <acchang4@gmail.com>
2023-12-30 20:03:30 -0500

    slow but ok

--- 1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters.js
@@ -5,13 +5,11 @@
 var maxLengthBetweenEqualCharacters = function(s) {
     let uniques = new Set(s)
     let output = -1
-    console.log(uniques)
     for (unique of uniques){
-        console.log(unique)
-        console.log(s.indexOf(unique), s.lastIndexOf(unique), s.lastIndexOf(unique)-1-s.indexOf(unique), output)
         if (s.indexOf(unique) != s.lastIndexOf(unique)){
-            if ((s.lastIndexOf(unique)-1 - s.indexOf(uniques)) > output){
-                output = (s.lastIndexOf(unique) - 1 - s.indexOf(unique))
+            let diff = s.lastIndexOf(unique) - (s.indexOf(unique)+1)
+            if (diff > output){
+                output = diff
             }
         }
     }
