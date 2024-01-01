32f80ec3784d173a4abfa6f26568c1bbca78a772
Andrew Chang <acchang4@gmail.com>
2023-12-31 19:41:10 -0500

    passes but 5.24% slow

--- 455-assign-cookies.js
+++ 455-assign-cookies.js
@@ -4,18 +4,12 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-    // maximize number of gratified kids
-    // maximize number of s>=g
-    // sort? they are really long
-    // 3 2 1
-    // 1 1
-    // if just match from end will be O(n)^2
     let output = 0
     let gSort = g.sort((a,b)=> b-a)
     let sSort = s.sort((a,b)=> b-a)
     for (let i=0;i<sSort.length;i++){
         for (let j=0; j<gSort.length;j++){
-            if (sSort[i]>=gSort[j]){output++, gSort[j] = Infinity}
+            if (sSort[i]>=gSort[j]){output++; gSort[j] = Infinity; break}
         }
     }
     return output
