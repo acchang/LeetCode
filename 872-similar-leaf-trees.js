f35c34f3fca07ea47b122edf7823615b8d3198d8
Andrew Chang <acchang4@gmail.com>
2024-01-08 19:35:43 -0500

    done

--- 872-similar-leaf-trees.js
+++ 872-similar-leaf-trees.js
@@ -1,4 +1,5 @@
-// https://leetcode.com/problems/leaf-similar-trees/?envType=daily-question&envId=2024-01-09
+// https://leetcode.com/problems/leaf-similar-trees/submissions/?envType=daily-question&envId=2024-01-09
+// https://www.youtube.com/watch?v=siOrghQosGU
 
 /**
  * Definition for a binary tree node.
@@ -13,19 +14,20 @@
  * @param {TreeNode} root2
  * @return {boolean}
  */
+
+
 var leafSimilar = function(root1, root2) {
     let leaf1 = []
     let leaf2 = []
 
     function dfs(node, leaf){
-        if (!node){return}
-        // if the node exists
+        if (!node){return} 
         if (!node.left && !node.right){
-            leaf.push(node.val)
+            leaf.push(node.val);
             return
         }
-        dfs(node.left)
-        dfs(node.right)
+        dfs(node.left, leaf)
+        dfs(node.right, leaf)
     }
 
     dfs(root1, leaf1);
@@ -33,3 +35,4 @@ var leafSimilar = function(root1, root2) {
 
     return leaf1.join("-") == leaf2.join("-") 
 };
+
