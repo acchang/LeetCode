30ce0efdcb11bf3951f2059149a2ef0ee9d7b2c7
Andrew Chang <acchang4@gmail.com>
2024-01-12 20:01:30 -0500

    add fast solution

--- 1348-minimum-number-of-steps-to-make-two-strings-anagram.js
+++ 1348-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -1,23 +1,68 @@
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/description/?envType=daily-question&envId=2024-01-13
+
+// passes but slow:
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {number}
  */
 var minSteps = function(s, t) {
-    // make t have same letters as s
-    // hashmap the two and determine differences 
-    // or just use includes and remove
     for (let i=0; i<s.length; i++){
-        console.log(s[i], t.includes(s[i]))
         if (t.includes(s[i])){
             let a = t.slice(0,t.indexOf(s[i]))
-            console.log(a)
             let b = t.slice(t.indexOf(s[i])+1)
-            console.log(b)
             t = a + b
-            console.log(t)
         }
     }
     return t.length
+};
 
+// better, per solution:
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    const mainMap = new Map();
+    for (let i=0; i<s.length; i++){
+        if (mainMap.has(s[i])){
+            mainMap.set(s[i], mainMap.get(s[i])+1)}
+        else {mainMap.set(s[i], 1)}
+
+        if (mainMap.has(t[i])){
+            mainMap.set(t[i], mainMap.get(t[i])-1)}
+        else {mainMap.set(t[i], -1)}
+    }
+
+    let ans = 0
+    
+    for (val of mainMap.values()) {
+        if (val >0){ans += val}
+        }
+
+    return ans
+};
+
+// superfast:
+// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/solutions/4555349/96-53-easy-solution-with-explanation/?envType=daily-question&envId=2024-01-13
+
+
+var minSteps = function(s, t) {
+    const countS = new Array(26).fill(0);
+    const countT = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        countS[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+        countT[t.charCodeAt(i) - 'a'.charCodeAt(0)]++;
+    }
+
+    let steps = 0;
+    for (let i = 0; i < 26; i++) {
+        steps += Math.abs(countS[i] - countT[i]);
+    }
+
+    return Math.floor(steps / 2);  
 };
