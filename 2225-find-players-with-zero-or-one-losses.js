ca7f65856bfce9bbf998d5ef6d8e943ef415fade
Andrew Chang <acchang4@gmail.com>
2024-01-14 19:44:10 -0500

    even better solution

--- 2225-find-players-with-zero-or-one-losses.js
+++ 2225-find-players-with-zero-or-one-losses.js
@@ -31,3 +31,49 @@ var findWinners = function(matches) {
     return [undefeated.sort((a,b)=>(a-b)),oneLoss.sort((a,b)=>(a-b))]
 
 };
+
+// 96% solution uses Sets, "collections of unique values"
+// better to use set than array or object bc can just use "has" instead of indexing to delete
+// https://leetcode.com/problems/find-players-with-zero-or-one-losses/solutions/4566935/beats-98-22-users-c-java-python-javascript-explained/?envType=daily-question&envId=2024-01-15
+
+/**
+ * @param {number[][]} matches
+ * @return {number[][]}
+ */
+var findWinners = function(matches) {
+    let zeroLoss = new Set();
+    let oneLoss = new Set();
+    let moreLoss = new Set();
+
+    for (let match of matches) {
+        let winner = match[0];
+        let loser = match[1];
+
+        // Add winner.
+        if (!oneLoss.has(winner) && !moreLoss.has(winner)) {
+            zeroLoss.add(winner);
+        }
+
+        // Add or move loser.
+        if (zeroLoss.has(loser)) {
+            zeroLoss.delete(loser);
+            oneLoss.add(loser);
+        } else if (oneLoss.has(loser)) {
+            oneLoss.delete(loser);
+            moreLoss.add(loser);
+        } else if (moreLoss.has(loser)) {
+            continue;
+        } else {
+            oneLoss.add(loser);
+        }
+    }
+
+    let answer = [
+        Array.from(zeroLoss).sort((a, b) => a - b),
+        Array.from(oneLoss).sort((a, b) => a - b)
+    ];
+    
+    return answer;
+};
+
+
