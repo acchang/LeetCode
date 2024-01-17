2317260812daaf52bf6068227a53754ddd96058b
Andrew Chang <acchang4@gmail.com>
2024-01-16 19:43:29 -0500

    done

--- 1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences.js
@@ -1,3 +1,27 @@
+https://leetcode.com/problems/unique-number-of-occurrences/?envType=daily-question&envId=2024-01-17
+
+// Daily Q 24/01/16
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    const arrMap = new Map()
+    for (entry of arr){
+        if (arrMap.has(entry)){arrMap.set(entry, arrMap.get(entry)+1)}
+        else arrMap.set(entry, 1)
+    }
+    let values = Array.from(arrMap.values())
+    let uniques = [...new Set(arrMap.values())]
+    
+    return values.length === uniques.length
+};
+
+
+/////// I did the one below earlier, it's slower. Makes sense why, so many operations.
+
+
 var uniqueOccurrences = function(arr) {
     // use newSet to get all uniques
         let uniques = [... new Set(arr)];
