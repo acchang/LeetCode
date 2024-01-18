19074c381b76e910eb5d2524bc8600e81be24a95
Andrew Chang <acchang4@gmail.com>
2024-01-18 17:21:37 -0500

    done

--- 70-climbing-stairs.js
+++ 70-climbing-stairs.js
@@ -8,8 +8,15 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if (n>=2){return n}
-    return climbStairs(n-1) + climbStairs(n-2) 
+
+    var dp = new Array(45)
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i=3; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    return dp[n]
 };
 
 
