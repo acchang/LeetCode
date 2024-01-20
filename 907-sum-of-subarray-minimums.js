18f37f5a8d24b0ee6d99b27c200f6b458953f2da
Andrew Chang <acchang4@gmail.com>
2024-01-19 20:47:42 -0500

    all passed but still too slow

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -6,11 +6,11 @@ var sumSubarrayMins = function(arr) {
     let res = 0
     for (let i=0; i<arr.length; i++){
         let currentMin = arr[i]
+        res += currentMin
         for (let j=i+1; j<arr.length; j++){
             currentMin = Math.min(currentMin, arr[j])
             res = res+currentMin
             }
         }
-    res = res + arr.reduce((acc, cv) => acc+ cv)
     return res % (Math.pow(10,9) + 7)
 };
