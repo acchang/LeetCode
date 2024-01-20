670f32c862b6b4b9d14289b297173e94997b25e2
Andrew Chang <acchang4@gmail.com>
2024-01-19 20:52:48 -0500

    monu solution also timed out

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -2,6 +2,29 @@
  * @param {number[]} arr
  * @return {number}
  */
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var sumSubarrayMins = function(arr) {
+    let res = 0
+    for (let i=0; i<arr.length; i++){
+        let currentMin = arr[i]
+        res += currentMin
+        let j = i+1
+        while (j<arr.length){
+            if (currentMin > arr[j]){
+                currentMin = arr[j]
+            }
+            res += currentMin
+            j++
+        }
+}
+    return res % (Math.pow(10,9) + 7)
+};
+
+OR 
+
 var sumSubarrayMins = function(arr) {
     let res = 0
     for (let i=0; i<arr.length; i++){
@@ -9,7 +32,7 @@ var sumSubarrayMins = function(arr) {
         res += currentMin
         for (let j=i+1; j<arr.length; j++){
             currentMin = Math.min(currentMin, arr[j])
-            res = res+currentMin
+            res += currentMin
             }
         }
     return res % (Math.pow(10,9) + 7)
