046b5ed0d4211f5435989ea6e4a5139e7f1e35ab
Andrew Chang <acchang4@gmail.com>
2024-01-19 20:44:09 -0500

    passes initial tests

--- 907-sum-of-subarray-minimums.js
+++ 907-sum-of-subarray-minimums.js
@@ -4,11 +4,13 @@
  */
 var sumSubarrayMins = function(arr) {
     let res = 0
-        for (let i=0; i<arr.length; i++){
-            for (let j=i+1; j<arr.length+1; j++){
-                let section = arr.slice(i, j);
-                res = res + Math.min(...section);
+    for (let i=0; i<arr.length; i++){
+        let currentMin = arr[i]
+        for (let j=i+1; j<arr.length; j++){
+            currentMin = Math.min(currentMin, arr[j])
+            res = res+currentMin
             }
         }
+    res = res + arr.reduce((acc, cv) => acc+ cv)
     return res % (Math.pow(10,9) + 7)
 };
