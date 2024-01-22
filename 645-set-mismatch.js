86c15072fd5288d5bf41bdefc029861c9dee5641
Andrew Chang <acchang4@gmail.com>
2024-01-21 20:16:03 -0500

    done

--- 645-set-mismatch.js
+++ 645-set-mismatch.js
@@ -1,3 +1,48 @@
+
+// https://leetcode.com/problems/set-mismatch/solutions/2743032/js-99-100-with-explanation/?source=submission-noac
+// 94%
+
+// Now, passing through the array, we kind of sort it, so that our number from the nums array would be in the new array at the appropriate index ("- 1" - correction for the index). If this index already has a number from our array, then we got a duplicate, and write it to a variable, otherwise we put the number in place and subtract it from the sum.
+
+//  n(n+1)/2 is the formula to calculate sum of 'n' natural numbers, which is the sum of 1 to n.
+
+
+var findErrorNums = function(nums) {
+    let sum = (nums.length * (nums.length + 1)) / 2;
+    let dup = 0;
+    let arr = Array(nums.length).fill(0);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (arr[nums[i] - 1] === nums[i]) dup = nums[i];
+        else {
+            arr[nums[i] - 1] = nums[i];
+            sum -= nums[i];
+        }
+    }
+
+    return [dup, sum];
+};
+
+// also, without knowing the formula, get 88.20%
+
+var findErrorNums = function(nums) {
+    let dup = 0;
+    let arr = Array(nums.length).fill(0);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (arr[nums[i] - 1] === nums[i]) dup = nums[i];
+        else {
+            arr[nums[i] - 1] = nums[i];
+        }
+    }
+    
+    return [dup, arr.indexOf(0)+1]
+};
+
+
+
+/// this is 10%
+
 /**
  * @param {number[]} nums
  * @return {number[]}
