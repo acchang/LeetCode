1c2d25147a114ef498803cccda14f6238f886198
Andrew Chang <acchang4@gmail.com>
2024-01-27 19:57:32 -0500

    one liner

--- 2011-find-variable-after-performing-operations.js
+++ 2011-find-variable-after-performing-operations.js
@@ -8,7 +8,18 @@ var finalValueAfterOperations = function(operations) {
     let ans = 0
     for (op of operations){
         if (op == "++X" || op == "X++"){ans++}
-        if (op == "--X" || op == "X--"){ans++}
+        if (op == "--X" || op == "X--"){ans--}
     }
     return ans
 };
+
+/// this also works, but must bc --acc not acc--
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    return operations.reduce((acc, cV) => cV[1] === "+" ? ++acc : --acc, 0)
+};
+
