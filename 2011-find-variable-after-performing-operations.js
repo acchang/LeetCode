319f8ac2fdf40e5c7e7a7694f94c53cb37267576
Andrew Chang <acchang4@gmail.com>
2024-01-27 20:00:18 -0500

    prefix and postfix

--- 2011-find-variable-after-performing-operations.js
+++ 2011-find-variable-after-performing-operations.js
@@ -23,3 +23,24 @@ var finalValueAfterOperations = function(operations) {
     return operations.reduce((acc, cV) => cV[1] === "+" ? ++acc : --acc, 0)
 };
 
+/*
+https://stackoverflow.com/questions/6867876/javascript-i-vs-i
+
+++variable increments the variable, returning the new value.
+
+variable++ increments the variable, but returns the old value.
+
+--variable decrements the variable, returning the new value.
+
+variable-- decrements the variable, but returns the old value.
+
+For example:
+
+a = 5;
+b = 5;
+c = ++a;
+d = b++;
+a is 6, b is 6, c is 6 and d is 5.
+
+If you're not using the result, the prefix operators work equally to the postfix operators.
+*/
