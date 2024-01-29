69f2210875babe96c95bd5aed5c2110aee173461
Andrew Chang <acchang4@gmail.com>
2024-01-28 22:05:20 -0500

    tried

--- 232-implement-queue-using-stacks.js
+++ 232-implement-queue-using-stacks.js
@@ -1,5 +1,11 @@
 /* https://www.educative.io/answers/what-is-the-stack-data-structure-in-js
-https://leetcode.com/problems/implement-queue-using-stacks/description/ */
+https://leetcode.com/problems/implement-queue-using-stacks/description/ 
+
+Daily Question 1/28/24:
+https://leetcode.com/problems/implement-queue-using-stacks/description/?envType=daily-question&envId=2024-01-29
+old answer does not work
+*/
+
 
 
 var MyQueue = function() {
