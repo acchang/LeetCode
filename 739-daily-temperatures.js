13f431377ca950be0b926b51f5d7c868d4f6b17d
Andrew Chang <acchang4@gmail.com>
2024-01-31 05:46:50 -0500

    done

--- 739-daily-temperatures.js
+++ 739-daily-temperatures.js
@@ -1,4 +1,41 @@
 // https://leetcode.com/problems/daily-temperatures/
+// Daily question Jan 30 2024
+
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    let stack = [];
+    let ans = Array(temperatures.length).fill(0);
+// fill ans with zeroes for when it ends
+
+    for (let i=0; i<temperatures.length; i++){
+// go through temperatures
+        while(stack.length && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+// while there's something in the stack and temp at stack length is less than current temp
+        let index = stack.pop();
+// pop it off the top of the stack and call it index
+        ans[index] = i - index;
+// put into ans of index, the current minus that which you poppped.
+console.log(stack)
+console.log(ans)
+        }
+        stack.push(i);
+// nevertheless, push the curent index into stack
+    }
+    return ans;
+};
+
+
+
+
+
+
+
+
+
+
 
 /**
  * @param {number[]} temperatures
