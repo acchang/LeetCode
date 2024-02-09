1e77c2d48cccf954c9692727923f46250b5cee24
Andrew Chang <acchang4@gmail.com>
2024-02-08 21:11:20 -0500

    stuck 25/49

--- 368-largest-divisible-subset.js
+++ 368-largest-divisible-subset.js
@@ -8,15 +8,18 @@ var largestDivisibleSubset = function(nums) {
     nums.sort((a,b)=> a-b)
     let dp = [...Array(nums.length)].map(() => [])
     dp[0] = [nums[0]]
+    let temp = []
     for (let i=1; i<nums.length; i++){
         dp[i].push(nums[i])
         for (let j=i-1; j>-1; j--){
-            if (nums[i]%nums[j]==0){
-                dp[i].push(...dp[j]);
-                break
+            if ((nums[i]%nums[j]==0) && dp[j].length>=temp.length){
+                temp = dp[j];
+                console.log("temp", temp)
                 }
         }
+        dp[i].push(...temp)
     }
+    console.log(dp)
     dp.sort((a,b)=>b.length-a.length)
     return dp[0]
 };
