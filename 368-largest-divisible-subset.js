aabb3c51b2b3b53ffea490689b1cd6a588a0b860
Andrew Chang <acchang4@gmail.com>
2024-02-08 20:58:53 -0500

    fixed dp

--- 368-largest-divisible-subset.js
+++ 368-largest-divisible-subset.js
@@ -1,21 +1,22 @@
 // https://www.youtube.com/watch?v=bech4HH44Rs
 
-
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var largestDivisibleSubset = function(nums) {
     nums.sort((a,b)=> a-b)
-    let dp = []
-    for (let i=0; i<nums.length; i++){
-        let bracket = []
-        for (let j=0; j<i+1; j++){
-            if (nums[i]%nums[j]==0){bracket.push(nums[j])}
+    let dp = [...Array(nums.length)].map(() => [])
+    dp[0] = [nums[0]]
+    for (let i=1; i<nums.length; i++){
+        dp[i].push(nums[i])
+        for (let j=i-1; j>-1; j--){
+            if (nums[i]%nums[j]==0){
+                dp[i].push(...dp[j]);
+                break
+                }
         }
-        dp.push(bracket)
     }
-    console.log(dp)
     dp.sort((a,b)=>b.length-a.length)
     return dp[0]
 };
