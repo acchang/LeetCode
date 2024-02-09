88cd0437269b8229ddc7547a1439ffcee18bd734
Andrew Chang <acchang4@gmail.com>
2024-02-09 17:44:29 -0500

    15% solved

--- 368-largest-divisible-subset.js
+++ 368-largest-divisible-subset.js
@@ -1,5 +1,12 @@
-// https://www.youtube.com/watch?v=bech4HH44Rs
 
+// solved using ideas here:
+// https://www.youtube.com/watch?v=bech4HH44Rs
+// beats 15%
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -8,18 +15,16 @@ var largestDivisibleSubset = function(nums) {
     nums.sort((a,b)=> a-b)
     let dp = [...Array(nums.length)].map(() => [])
     dp[0] = [nums[0]]
-    let temp = []
     for (let i=1; i<nums.length; i++){
         dp[i].push(nums[i])
+        let temp = []
         for (let j=i-1; j>-1; j--){
             if ((nums[i]%nums[j]==0) && dp[j].length>=temp.length){
                 temp = dp[j];
-                console.log("temp", temp)
                 }
         }
         dp[i].push(...temp)
     }
-    console.log(dp)
     dp.sort((a,b)=>b.length-a.length)
     return dp[0]
 };
