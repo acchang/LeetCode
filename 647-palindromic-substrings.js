5de75c97f05b4b629f4b2315a24da0c84522bf12
Andrew Chang <acchang4@gmail.com>
2024-02-10 05:54:27 -0500

    done

--- 647-palindromic-substrings.js
+++ 647-palindromic-substrings.js
@@ -6,6 +6,22 @@
  * @param {string} s
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var countSubstrings = function(s) {
+    let res = 0
+    for (let i = 0; i<s.length; i++){
+        for (let left = i, right = i; left > -1 && right <s.length; left--, right++){
+            if (s[left] == s[right]){res++} else break
+        }
+    }
     
+    for (let j = 0; j<s.length; j++){
+        for (let left = j, right = j+1; left > -1 && right <s.length; left--, right++){
+            if (s[left] == s[right]){res++} else break
+        }
+    }
+    return res
 };
