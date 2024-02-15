e525256d19743551af6f315a8666e1aa716da4aa
Andrew Chang <acchang4@gmail.com>
2024-02-14 19:33:04 -0500

    book solution 80%, avoids reduce

--- 2971-find-polygon-with-the-largest-perimeter.js
+++ 2971-find-polygon-with-the-largest-perimeter.js
@@ -1,3 +1,7 @@
+// https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/description/?envType=daily-question&envId=2024-02-15
+// DQ 2/14/24
+// 25%
+
 /**
  * @param {number[]} nums
  * @return {number}
@@ -12,3 +16,19 @@ var largestPerimeter = function(nums) {
     }
     return -1
 };
+
+// book solution is 80%
+
+var largestPerimeter = function(nums) {
+    nums.sort((a, b) => a - b);
+    let previousElementsSum = 0;
+    let ans = -1;
+    for (let num of nums) {
+        if (num < previousElementsSum) {
+            ans = num + previousElementsSum;
+        }
+        previousElementsSum += num;
+    }
+    return ans;
+};
+
