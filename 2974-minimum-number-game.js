dea9db9132afb54461972b2b7dd1e49a3d5d24d2
Andrew Chang <acchang4@gmail.com>
2024-02-17 10:10:14 -0500

    done 85%

---
+++ 2974-minimum-number-game.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    let arr = []
+    nums.sort((a,b)=>a-b)
+    for (let i=0; i<nums.length; i=i+2){
+        arr.push(nums[i+1])
+        arr.push(nums[i])
+    }
+    return arr
+};
