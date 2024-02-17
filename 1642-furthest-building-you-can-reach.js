d85a862d85e238f2f4467e62808a9862518f2e70
Andrew Chang <acchang4@gmail.com>
2024-02-17 09:57:41 -0500

    do later

--- 1642-furthest-building-you-can-reach.js
+++ 1642-furthest-building-you-can-reach.js
@@ -1,3 +1,7 @@
+// heap is the solution but this is too advanced for now
+// try easier heap qs
+// 2/16/24 DQ
+
 /**
  * @param {number[]} heights
  * @param {number} bricks
@@ -5,21 +9,87 @@
  * @return {number}
  */
 
+/// book answer
 
-/// this fails on something like when the gaps are like this:
-// 4 1 1 1 1 9999
-// ladder first. I need to learn heaps to solve.
+var furthestBuilding = function(h, b, l) {
+    const p = new MaxHeap();
+    
+    let i = 0, diff = 0;
+    for (i = 0; i < h.length - 1; i++) {
+        diff = h[i + 1] - h[i];
+        
+        if (diff <= 0) {
+            continue;
+        }
 
-var furthestBuilding = function(heights, bricks, ladders) {
-    for (let i=0; i<heights.length; i++){
-        console.log("i",i)
-        if ((heights[i+1]-heights[i]>bricks) && ladders == 0){return i}
-        if (heights[i]>heights[i+1]){console.log(i); continue}
-        else if (heights[i+1]-heights[i]<=bricks){
-            bricks -= (heights[i+1]-heights[i]);
-                        console.log("br", bricks, i)}
-        else if (ladders>0){ladders--;
-        console.log("lad", ladders, i)}
+        b -= diff;
+        p.push(diff);
+
+        if (b < 0) {
+            b += p.pop();
+            l--;
+        }
+
+        if (l < 0) {
+            break;
+        }
     }
-    return heights.length-1
+    
+    return i;
 };
+
+class MaxHeap {
+    constructor() {
+        this.heap = [];
+    }
+    
+    push(val) {
+        this.heap.push(val);
+        this.heapifyUp();
+    }
+    
+    pop() {
+        const max = this.heap[0];
+        const last = this.heap.pop();
+        
+        if (this.heap.length > 0) {
+            this.heap[0] = last;
+            this.heapifyDown();
+        }
+        
+        return max;
+    }
+    
+    heapifyUp() {
+        let current = this.heap.length - 1;
+        while (current > 0) {
+            const parent = Math.floor((current - 1) / 2);
+            if (this.heap[parent] >= this.heap[current]) {
+                break;
+            }
+            [this.heap[parent], this.heap[current]] = [this.heap[current], this.heap[parent]];
+            current = parent;
+        }
+    }
+    
+    heapifyDown() {
+        let current = 0;
+        while (true) {
+            let leftChild = 2 * current + 1;
+            let rightChild = 2 * current + 2;
+            let maxChild = leftChild;
+            
+            if (rightChild < this.heap.length && this.heap[rightChild] > this.heap[leftChild]) {
+                maxChild = rightChild;
+            }
+            
+            if (leftChild >= this.heap.length || this.heap[current] >= this.heap[maxChild]) {
+                break;
+            }
+            
+            [this.heap[current], this.heap[maxChild]] = [this.heap[maxChild], this.heap[current]];
+            current = maxChild;
+        }
+    }
+}
+
