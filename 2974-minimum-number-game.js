9cf7c73976359a20aaca5a83f11f29c1b18d4ca1
Andrew Chang <acchang4@gmail.com>
2024-02-17 10:14:32 -0500

    simple heap

--- 2974-minimum-number-game.js
+++ 2974-minimum-number-game.js
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/minimum-number-game/description/
+// 85%
+
 /**
  * @param {number[]} nums
  * @return {number[]}
@@ -10,4 +13,48 @@ var numberGame = function(nums) {
         arr.push(nums[i])
     }
     return arr
+};
+
+// 23%
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const numberGame = (nums) => {
+    nums.sort((a, b) => b - a);
+  
+    const arr = [];
+  
+    while (nums.length) {
+      const alice = nums.pop();
+      const bob = nums.pop();
+  
+      arr.push(bob, alice);
+    }
+  
+    return arr;
+  };
+
+// heap, 52%
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var numberGame = function(nums) {
+    let bobMin = 0;
+    let aliMin = 0;
+    let arr = [];
+    while(nums.length){
+        aliMin = Math.min(...nums);
+        let index = nums.indexOf(aliMin)
+        let aliRemoved = nums.splice(index, 1)[0];
+        bobMin = Math.min(...nums);
+        index = nums.indexOf(bobMin)
+        let bobRemoved = nums.splice(index, 1)[0];
+        arr.push(bobRemoved);
+        arr.push(aliRemoved);
+    }
+    return arr;
 };
