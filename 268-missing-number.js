063225281c5f03f1d6c825f49889300adb05c91c
Andrew Chang <acchang4@gmail.com>
2024-02-19 20:20:03 -0500

    done

--- 268-missing-number.js
+++ 268-missing-number.js
@@ -13,4 +13,29 @@ var missingNumber = function(nums) {
     return nums.length
 };
 
+// Math formula solution, fastest, 60%
+
+var missingNumber = function(nums) {
+    let n = nums.length
+    let sum = n*(n+1)/2
+    let arraysum = nums.reduce((acc, cv)=>acc+cv)
+    return sum - arraysum
+};
+
+// hash table solution
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    const map1 = new Map();
+    for (num of nums){
+        map1.set(num, nums.indexOf(num))
+    }
+    for (let i=0; i<nums.length; i++){
+        if (map1.get(i) == null){return i}
+    }
+    return nums.length
+};
 
