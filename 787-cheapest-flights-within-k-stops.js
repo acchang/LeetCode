c061a32f456a433836c2fd8b96c73ef799977d7c
Andrew Chang <acchang4@gmail.com>
2024-02-23 15:35:55 -0500

    monu solution

--- 787-cheapest-flights-within-k-stops.js
+++ 787-cheapest-flights-within-k-stops.js
@@ -1,4 +1,34 @@
 // https://leetcode.com/problems/cheapest-flights-within-k-stops/?envType=daily-question&envId=2024-02-23
+// https://www.youtube.com/watch?v=rPuNr0D97hg
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    let adj = new Array(n)
+    for (let i=0; i<n; i++){
+        adj[i] = new Array
+    }
+    for (let [from, to, price] of flights){
+        adj[from].push([to,price])
+    }
+
+let memo = new Array(n).fill(Infinity)
+let queue = [[src, 0]]
+
+while (queue.length && k> -1){
+    let sz = queue.length
+    while (sz--) {
+        let [node, price] = queue.shift()
+        for (let [neighbor, cost] of adj[node]){
+            if (memo[neighbor] < price + cost) continue
+            memo[neighbor] = price + cost
+            queue.push([neighbor, memo[neighbor]])
+        }
+    }
+    k--
+}
+
+return memo[dst] == Infinity ? -1: memo[dst]
+}
+
+
 // https://www.youtube.com/watch?v=5eIK3zUdYmE&t=1213s
 
 /**
