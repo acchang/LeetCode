af44b7f3637337a52aba31eab03034d48a9b4fa7
Andrew Chang <acchang4@gmail.com>
2024-02-27 19:01:15 -0500

    unsolved

---
+++ 504-diameter-of-binary-tree.js
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/diameter-of-binary-tree/?envType=daily-question&envId=2024-02-27
+// Daily Q: 2/27/24
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    if (!root){return}
+    
+};
