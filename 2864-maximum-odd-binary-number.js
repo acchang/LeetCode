216db45cb357fbd9407bb836386239ff3fa43aab
Andrew Chang <acchang4@gmail.com>
2024-02-29 20:07:15 -0500

    2 pointer solution, 90%

--- 2864-maximum-odd-binary-number.js
+++ 2864-maximum-odd-binary-number.js
@@ -15,3 +15,50 @@ var maximumOddBinaryNumber = function(s) {
 
 };
 
+
+// two pointer solution, left pointer at leftmost, right at rightmost
+// if leftmost is 1, move on; if rightmost is 0 move on
+// if not, then that pointer freezes and the other moves on until they're opposite
+// then they swap
+// finally they swap the last of the string and the last of the 1s to put a 1 at the end.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var maximumOddBinaryNumber = function(s) {
+    // Create char array
+         let arr = s.split('')
+         let N = arr.length;
+         
+         // Initialize two pointers
+         let left = 0;
+         let right = N - 1;
+ 
+         while (left <= right) {
+             
+             // Increment left if equals 1
+             if (arr[left] == '1') {
+                 left++;
+             }
+             // Decrement right if equals 0
+             if (arr[right] == '0') {
+                 right--;
+             }
+             // Swap if neither pointer can be iterated
+             if (left <= right && arr[left] == '0' && arr[right] == '1') {
+                 arr[left] = '1';
+                 arr[right] = '0';
+             }
+         }
+ 
+         // Swap rightmost 1 bit to the end
+         arr[left - 1] = '0';
+         arr[N - 1] = '1';
+ 
+         return arr.join('');
+     };
+ 
+
+
+     
