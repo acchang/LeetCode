af73f59cc2dab02ad871900674f599ace57224f6
Andrew Chang <acchang4@gmail.com>
2024-03-01 20:11:30 -0500

    pointers

--- 977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array.js
@@ -10,3 +10,28 @@ var sortedSquares = function(nums) {
     for (num of nums){ans.push(num**2)}
     return ans.sort((a,b)=>(a-b))
 };
+
+// pointers solution 40% better than previous 28%
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let start = 0
+    let end = nums.length - 1
+    let index = end
+    let newSortArr =[]
+
+    while (index>-1){
+        if (Math.abs(nums[start]) > Math.abs(nums[end])){
+            newSortArr[index--] = (nums[start]**2);
+            start++;
+        } else {
+            newSortArr[index--] = (nums[end]**2)
+            end--;
+        }
+    }
+    return newSortArr
+};
+
