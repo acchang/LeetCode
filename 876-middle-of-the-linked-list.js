13feca3f60dfd4b35604d120e68b165e31f210cd
Andrew Chang <acchang4@gmail.com>
2024-03-06 19:24:56 -0500

    done

--- 876-middle-of-the-linked-list.js
+++ 876-middle-of-the-linked-list.js
@@ -1,3 +1,30 @@
+// https://leetcode.com/problems/middle-of-the-linked-list/description/?envType=daily-question&envId=2024-03-07
+// DQ 3/6/24
+// solved on my own
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let fast = head
+    while (fast && fast.next){
+    head = head.next;
+    fast = fast.next.next;
+}
+return head
+};
+
+
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
