7cfc41a85c96434b737ec0d41d84e287d8ef0233
Andrew Chang <acchang4@gmail.com>
2024-03-08 19:34:39 -0500

    set and filter method

--- 2540-minimum-common-value.js
+++ 2540-minimum-common-value.js
@@ -1,3 +1,6 @@
+// https://leetcode.com/problems/minimum-common-value/description/?envType=daily-question&envId=2024-03-09
+// dq 3/8/24
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
@@ -12,4 +15,18 @@ var getCommon = function(nums1, nums2) {
         if (nums2[twoPointer] < nums1[onePointer]){twoPointer++}
     }
     return -1
+};
+
+// using set:
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var getCommon = function(nums1, nums2) {
+    const set = new Set(nums1);
+    const common = nums2.filter(n => set.has(n)).sort((a,b) => a - b)[0];
+    
+    return common !== undefined ? common : -1;
 };
