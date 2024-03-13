1a2c17bd0ba459e253615bbd083c4546f8d10664
Andrew Chang <acchang4@gmail.com>
2024-03-12 20:29:38 -0400

    starting

---
+++ 2485-find-the-pivot-integer.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var pivotInteger = function(n) {
+    // two pointers, add total
+    // the lesser one advances
+    // while one side != other
+    
+};
